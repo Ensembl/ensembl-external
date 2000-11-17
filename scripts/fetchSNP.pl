10360ffeccfa85645a8e20533c3657a361c2d326
heikki <heikki@sanger.ac.uk>
2000-11-17 09:17:51 +0000

    basic SNP retrieval example

---
+++ scripts/fetchSNP.pl
@@ -0,0 +1,45 @@
+#!/usr/local/bin/perl -w
+
+use Bio::EnsEMBL::ExternalData::SNPSQL::DBAdapter;
+use Bio::EnsEMBL::ExternalData::Variation;
+#use strict;
+
+#creating the object
+my $snpdb = Bio::EnsEMBL::ExternalData::SNPSQL::DBAdapter
+    ->new( -dbname=>'snp', 
+	   -user=>'root',
+	   #-host=>'ecs1b.sanger.ac.uk'
+	   );
+
+# using the method get_SeqFeature_by_id
+
+my $id = shift;
+
+$id = "5" unless defined $id;
+
+my @snps = $snpdb->get_SeqFeature_by_id($id);
+my $snp = pop @snps;
+
+print 
+"\nID          : ",        $snp->id,
+"\nseqname     : ",        $snp->seqname,
+"\nstart       : ",        $snp->start,
+"\nend         : ",        $snp->end,
+"\nstrand      : ",        $snp->strand,
+"\nsource_tag  : ",        $snp->source_tag,
+"\nscore       : ",        $snp->score,
+"\nstatus      : ",        $snp->status,
+"\nupStreamSeq : ",        $snp->upStreamSeq,
+"\nalleles     : ",        $snp->alleles,
+"\ndnStreamSeq : ",        $snp->dnStreamSeq,
+"\n";
+
+foreach my $link ( $snp->each_DBLink ) {
+
+print 
+"DBLink      : ",        $link->database, "::", $link->primary_id,
+"\n";
+
+}
+#$snpdb->disconnect;
+#undef $snpdb;

b011816f909a138cab25125981cc04b71576869c
Philip Lijnzaad <lijnzaad@sanger.ac.uk>
2001-02-07 13:19:30 +0000

    added stuff to add ensembl gene id's. Currently they are looked up in a hardcoded database (it warns about this). They are on top of the peptide ids (but this can be switched easily by setting switches in the code, towards top).

--- scripts/family-input.pl
+++ scripts/family-input.pl
@@ -9,12 +9,20 @@ use DBI;
 use strict;
 
 use vars qw($opt_h $opt_r $opt_U $opt_H $opt_D $opt_C $opt_r $release 
-            $opt_C $ddl $max_desc_len $enspep_name $fam_id_format);
+            $opt_C $ddl $max_desc_len 
+            $enspep_dbname $ensgene_dbname $sp_dbname
+            $fam_id_format);
 
 use Getopt::Std;
 
-$max_desc_len = 255; 
-$enspep_name = 'ENSEMBLPEP';
+$max_desc_len = 255;                    # max length of description
+$enspep_dbname = 'ENSEMBLPEP';          # name of EnsEMBL peptides database
+$ensgene_dbname = 'ENSEMBLGENE';        # name of EnsEMBL peptides database
+$sp_dbname = 'SWISSPROT';               # name of SWISSPROT database
+my $add_ens_pep =1;                     # should ens_peptides be added
+my $add_ens_gene =1;                    # should ens_genes be added
+my $add_sp =1;                          # should swissprot entries be added
+
 $fam_id_format= 'ENSF%011d';
 
 my $usage = 
@@ -31,14 +39,16 @@ my $database = ($opt_D || 'family');
 my $release = ($opt_r || die " need a release number\n");
 my $ddl = $opt_C;
 
-sub _create_db {
+$ddl = '../sql/family.sql' if $ddl == 1;
+
+sub create_db {
     my ($dbh) = @_;
     $dbh->do("CREATE DATABASE $database");
     warn "problem creating database: " . $DBI::errstr if $DBI::err;
 }
 
 ### Aargggh! 
-sub _create_tables { 
+sub create_tables { 
     my ($dbh, $ddl) = @_;
     my $q = `cat $ddl`;
 
@@ -50,7 +60,7 @@ sub _create_tables {
  
 
 # from database
-sub _get_max_id { 
+sub get_max_id { 
     my($dbh) = @_; 
     my $query = 
       "SELECT MAX(internal_id) AS id 
@@ -67,12 +77,48 @@ sub _get_max_id {
 }
 
 # create id for format
-sub _format_fam_id {
+sub format_fam_id {
     my $num = shift;
     sprintf $fam_id_format, $num;
 }
 
+sub open_ensembl { 
+warn "using hardcoded host, db and user here";
+     my ($host, $user, $db) = ('ensrv3', 'ensro', 'simon_oct07');
+#     my ($host, $user, $db) = ('ecs1b', 'ensro', 'arne_ensembl_main');
+
+    my $dbh=DBI->connect("DBI:mysql:database=$db;host=$host", $user) ||
+      die "couldn't connect to $db at $host as $user:" . $DBI::errstr;
+    $dbh;
+}
+
+sub ens_gene_of {
+    my ($q, $pepid) = @_;
+    $q->execute($pepid);
+
+    my $rowhash;
+    my @results;
+    while ( $rowhash = $q->fetchrow_hashref) {
+        push @results, $rowhash->{id};
+    }
+    die "couldn't execute:" . $q->err if $q->errstr;
+    if (@results != 1) {
+        if (@results){
+            warn "for $pepid, I got these genes: " 
+              . join(':', @results), "\n"; 
+            die "not exactly one gene";
+        } else {
+            warn "for $pepid, I got no genes\n";
+            # die "not exactly one gene"; # ignore for now
+        }
+    }
+    my $id =     $results[0];
+    warn "gene  is: $id \n";
+    $id;
+}
+
 sub main {
+warn "checking for /^COBP/ too ...";
     my $fam_count = 0;
     my $mem_count =0;
     my $dsn;
@@ -85,13 +131,13 @@ sub main {
         $dsn = "DBI:mysql:database=mysql;host=$host";
         $dbh = DBI->connect("$dsn",'root') ||
           die "couldn't connect using $dsn and 'root':" . $DBI::errstr;
-        _create_db($dbh, $ddl);
+        create_db($dbh, $ddl);
         $dbh->disconnect;
 
         $dsn = "DBI:mysql:database=$database;host=$host";
         $dbh = DBI->connect("$dsn",'root') ||
           die "couldn't connect using $dsn and 'root':" . $DBI::errstr;
-        _create_tables($dbh, $ddl);
+        create_tables($dbh, $ddl);
 
         warn "created...";
     }
@@ -101,23 +147,34 @@ sub main {
       die "couldn't connect using $dsn and user $user:" . $DBI::errstr;
     $dbh->{autocommit}++;
 
-    $internal_id = _get_max_id($dbh) +1;
 
+    # separate handle for looking up translation from enspep to ensgene:
+    my $ensdb = open_ensembl();
 
+    $internal_id = get_max_id($dbh) +1;
     my $fam_q = 
       "INSERT INTO family(internal_id, id, description, release, 
                          annotation_confidence_score)
        VALUES(?,            ?,         ?,        ?,        ?)\n";
 ###            $internal_id, '$fam_id', '$descr', $release, $score);
 
-    my $fam_q = $dbh->prepare($fam_q) || die $dbh->errstr;
+    $fam_q = $dbh->prepare($fam_q) || die $dbh->errstr;
 
     my $mem_q = 
       "INSERT INTO family_members(family, db_name, db_id)
                            VALUES(?,      ?,       ?)\n";
 ###                         $internal_id, '$db_name', '$mem'
 
-    my $mem_q = $dbh->prepare($mem_q) || die $dbh->errstr;
+    $mem_q = $dbh->prepare($mem_q) || die $dbh->errstr;
+
+
+    my $gene_q = 
+      "SELECT g.id 
+       FROM gene g, translation tl, transcript tc 
+       WHERE tl.id = ? 
+           AND tl.id = tc.translation and tc.gene = g.id";
+
+    $gene_q = $ensdb->prepare($gene_q) || die $dbh->errstr;
 
     while(<>) {
         chomp;
@@ -138,22 +195,42 @@ sub main {
             $descr = substr($descr, 0, $max_desc_len);
         }
         
-        my $fam_id = _format_fam_id $num;
+        my $fam_id = format_fam_id $num;
         
         $fam_q->execute($internal_id, $fam_id, $descr, $release, $score)
           || die "couldn't insert line $.:\n$_\n " . $fam_q->errstr;
-        
-        foreach my $mem (@mems) {
-            my $db_name = ( $mem =~ /^ENSP/ )? $enspep_name : 'SWISSPROT';
-                                        # or SPTREMBL, or SPALL ? 
 
-            $mem_q->execute($internal_id, $db_name, $mem)
-              || die "couldn't insert line $.:\n$_\n " . $mem_q->errstr;
-            $mem_count++;
-        }
+        my %seen_gene = undef;
+        foreach my $mem (@mems) {
+            if ($mem =~ /^ENSP/ || $mem =~ /^COBP/ ) {
+                if ($add_ens_pep)  {
+                    $mem_q->execute($internal_id, $enspep_dbname, $mem)
+                      || die "couldn't insert line $.:\n$_\n " . $mem_q->errstr;
+                    $mem_count++;
+                }
+                
+                if ($add_ens_gene) {
+                    my $ens_gene_id = ens_gene_of($gene_q, $mem);
+
+                    if ( ! $ens_gene_id 
+                         || defined $seen_gene{$ens_gene_id}) {
+                    } else { 
+                        $seen_gene{$ens_gene_id}++;                        
+                        $mem_q->execute($internal_id, $ensgene_dbname, 
+                                        $ens_gene_id)
+                          || die "couldn't insert line $.:\n$_\n " . $mem_q->errstr;
+                        $mem_count++;
+                    }
+                }
+            } else {                    # swissprot entry
+                $mem_q->execute($internal_id, $sp_dbname, $mem)
+                  || die "couldn't insert line $.:\n$_\n " . $mem_q->errstr;
+                $mem_count++;
+            }
+        }                               # foreach mem
         $internal_id++;
         $fam_count++;
-    }
+    }                                   # foreach fam
 
     $dbh->disconnect;
     warn "inserted $fam_count families, having a total of $mem_count members\n";

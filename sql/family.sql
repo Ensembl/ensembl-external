7b77aae1f51ce31e0f38de709b5f815c7a3a82ec
Philip Lijnzaad <lijnzaad@sanger.ac.uk>
2001-04-26 14:23:35 +0000

    added table cumulative_distrib

--- sql/family.sql
+++ sql/family.sql
@@ -9,6 +9,8 @@ CREATE TABLE family (
    description   varchar(255) NOT NULL,
    release       varchar(10) NOT NULL,
    annotation_confidence_score double, 
+
+   num_ens_pepts int(10),
     
    PRIMARY KEY(internal_id), 
    UNIQUE KEY(id)
@@ -19,10 +21,16 @@ CREATE TABLE family_members (
   db_name             varchar(12) NOT NULL, 
                                  ## currently ENSEMBLPEP, SPTREMBL, SWISSPROT
   db_id               varchar(40) NOT NULL, ##  e.g., ENSP000001234 or P12345
-##  score double ? -- ie., some confidence in likelyhood of cluster assignment ?
     PRIMARY KEY(family, db_name, db_id),
     UNIQUE KEY(db_name, db_id)
 );
+
+CREATE TABLE cumulative_distrib (
+  family_size int(10) NOT NULL,
+  occurrences int(10) NOT NULL,
+  cum_fraction_of_peptides float(4) NOT NULL, 
+  PRIMARY KEY(family_size)
+); 
 
 ### not yet implemented:
 # ## table to hold keywords

b4437d842052ffc374439d051a69cc211062ee49
Philip Lijnzaad <lijnzaad@sanger.ac.uk>
2001-08-28 15:58:01 +0000

    also max

--- scripts/fam-stats.sh
+++ scripts/fam-stats.sh
@@ -25,11 +25,11 @@ grep "^### replacing " $file | wc -l
 leaving=`grep "^### leaving " $file | wc -l`
 notbetter=`grep "^### alternative " $file | wc -l`
 echo "conflicts resolved by choosing first assignment:" $notbetter
-echo "undecided conflicts (first assignment kept)" $[leaving - notbetter]
+echo "undecided conflicts (first assignment kept):" $[leaving - notbetter]
 
 echo
 
-brackets="1-1 2-10 11-100 101-1000 1001-10000"
+brackets="0-0 1-1 2-10 11-100 101-1000 1001-10000"
 
 echo "Histogram of family sizes:"
 for b in $brackets; do
@@ -38,4 +38,10 @@ for b in $brackets; do
      '{print "select sum(occurrences) from cumulative_distrib where family_size between ",$1," and ",$2}' |\
         mysql -u $user -h $host $dbname | grep -v 'sum'
 done
+
+max=`echo 'select max(num_ens_pepts) from family' |\
+    mysql -N --batch -u $user -h $host $dbname `
+echo "Largest family has  $max ensembl members. Row is:"
+echo "select * from family where num_ens_pepts = $max;" |\
+    mysql -u $user -h $host $dbname
 

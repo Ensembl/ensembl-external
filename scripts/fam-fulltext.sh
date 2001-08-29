612e07b49432f7e6ca82d7fdbd462121abd6ebf9
Philip Lijnzaad <lijnzaad@sanger.ac.uk>
2001-08-29 16:36:23 +0000

    sql code  now moved into fam-fulltext.sh

--- scripts/fam-fulltext.sh
+++ scripts/fam-fulltext.sh
@@ -4,15 +4,25 @@
 # script producing file suitable for Tony's AltaVista indexing
 # Used by wrapper script fam-fulltext.sh
 
-usage="Usage: $0 mysql-args >  file.txt"
+usage="Usage: $0 -h host -u user databasename >  file.txt"
 
-if [ $# -lt 1 ] ; then
+if [ $# -lt 5 ] ; then
     echo $usage
     exit 1
 fi
 
 sql=./fam-fulltext.sql
 mysql=mysql
+mysql_extra_flags='--batch'
 
-$mysql "$@" < $sql
+(cat <<EOF
+# SQL for producing file suitable for Tony's AltaVista indexing
+# Used by wrapper script fam-fulltext.sh
+SELECT CONCAT(fm.db_id, '|family|', f.description) 
+FROM family_members fm, family f 
+WHERE fm.family = f.internal_id 
+  AND fm.db_name='ENSEMBLGENE' 
+EOF
+) | $mysql $mysql_extra_flags "$@"
+# ) | cat
 exit 0
--- scripts/fam-fulltext.sql
+++
@@ -1,9 +0,0 @@
-# $Id$
-# SQL for producing file suitable for Tony's AltaVista indexing
-# Used by wrapper script fam-fulltext.sh
-SELECT CONCAT(fm.db_id, '|family|', f.description) 
-FROM family_members fm, family f 
-WHERE fm.family = f.internal_id 
-  AND fm.db_name='ENSEMBLGENE' 
-
-

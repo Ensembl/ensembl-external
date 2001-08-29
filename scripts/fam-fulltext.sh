0914f29eb976e4dfaa7c5750d130267c90c1be76
Philip Lijnzaad <lijnzaad@sanger.ac.uk>
2001-08-29 16:46:54 +0000

    *** empty log message ***

--- scripts/fam-fulltext.sh
+++ scripts/fam-fulltext.sh
@@ -17,7 +17,6 @@ mysql_extra_flags='--batch'
 
 (cat <<EOF
 # SQL for producing file suitable for Tony's AltaVista indexing
-# Used by wrapper script fam-fulltext.sh
 SELECT CONCAT(fm.db_id, '|family|', f.description) 
 FROM family_members fm, family f 
 WHERE fm.family = f.internal_id 

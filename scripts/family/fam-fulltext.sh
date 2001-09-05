f1c554dff884bdcb2122601653df758559e45ca6
Philip Lijnzaad <lijnzaad@sanger.ac.uk>
2001-09-05 14:54:43 +0000

    renamed fam-fulltext.sh to family/fam-fulltext.sh

---
+++ scripts/family/fam-fulltext.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# $Id$
+
+# script producing file suitable for Tony's AltaVista indexing
+
+usage="Usage: $0 -h host -u user databasename >  file.txt"
+
+if [ $# -lt 5 ] ; then
+    echo $usage
+    exit 1
+fi
+
+sql=./fam-fulltext.sql
+mysql=mysql
+mysql_extra_flags='--batch'
+
+(cat <<EOF
+# SQL for producing file suitable for Tony's AltaVista indexing
+SELECT CONCAT(fm.db_id, '|family|', f.description) 
+FROM family_members fm, family f 
+WHERE fm.family = f.internal_id 
+  AND fm.db_name='ENSEMBLGENE' 
+EOF
+) | $mysql $mysql_extra_flags "$@"
+# ) | cat
+exit 0

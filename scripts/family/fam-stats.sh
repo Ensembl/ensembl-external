09c0a07cb4ce482aab86f7d4255a83c1bd984b9a
Philip Lijnzaad <lijnzaad@sanger.ac.uk>
2001-09-05 14:54:48 +0000

    renamed fam-stats.sh to family/fam-stats.sh

---
+++ scripts/family/fam-stats.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+# $Id$
+
+# Some statistics on families
+
+usage="Usage: $0 host database user family-input.log"
+if [ $# -ne 4  ]; then
+     echo $usage; exit 1;
+fi
+
+host=$1
+dbname=$2
+user=$3
+file=$4
+
+echo -n Totals:
+grep "^inserted" $file
+
+echo -n 'Total number of "one gene, several family" conflicts':
+grep "^### assigning" $file| wc -l
+
+echo -n "conflicts resolved by choosing second (or later) assignment:"
+grep "^### replacing " $file | wc -l
+
+leaving=`grep "^### leaving " $file | wc -l`
+notbetter=`grep "^### alternative " $file | wc -l`
+echo "conflicts resolved by choosing first assignment:" $notbetter
+echo "undecided conflicts (first assignment kept):" $[leaving - notbetter]
+
+echo
+
+brackets="0-0 1-1 2-10 11-100 101-1000 1001-10000"
+
+echo "Histogram of family sizes (as far as ensembl peptides are concerned):"
+for b in $brackets; do
+    echo -n "$b: "
+    echo $b | awk -F'-' \
+     '{print "select sum(occurrences) from cumulative_distrib where family_size between ",$1," and ",$2}' |\
+        mysql -u $user -h $host $dbname | grep -v 'sum'
+done
+
+max=`echo 'select max(num_ens_pepts) from family' |\
+    mysql -N --batch -u $user -h $host $dbname `
+echo "Largest family has  $max ensembl members. Row is:"
+echo "select * from family where num_ens_pepts = $max;" |\
+    mysql -u $user -h $host $dbname
+

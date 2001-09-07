441c9b5f850d2cf5d4c5d9ed114d546b283adef4
Philip Lijnzaad <lijnzaad@sanger.ac.uk>
2001-09-07 15:18:56 +0000

    *** empty log message ***

--- family/scripts/clean-blast.pl
+++ family/scripts/clean-blast.pl
@@ -1,43 +1,39 @@
 #!/usr/local/bin/perl
+# $Id$
+
+# Usage: clean-blast.pl < file(s) > otherfile
+
 use POSIX;
 use strict;
 # throws away unnecessary  cruft from raw blast output
 
 my $total=0;
-# open (FILE,$ARGV[0]);
+
+my ($last1, $last2);
 LINE:
 while(<>) {
-    my ($protein1, $protein2, $first, $second, $last1, $last2);
     if ( /^PARSED>\s*(\d+)/ ) {
         $total += $1;
         next LINE;
     }
-    if (/^(\S+)\s+(\S+)/) {
-        $protein1=$1;
-        $protein2=$2;
-    }
-    if (/([\S+]?)e-(\d+)/) {
-        chop($_);
-        $first=$1;
-        $second=$2;
-        # $text=join("e-","$first","$second");
-        # @rest=split(/$text/,$_);
-        
-        if ($first eq '') {
-            $first=1;	
-        }
+    my ($protein1, $protein2, $a, $b, $score) = split(' ');
+    my ($factor, $magnitude);
 
-        if (($last1 ne $protein1) || ($last2 ne $protein2)) {
-            print "$protein1\t$protein2\t$first\t$second\n";
-        }
-
-        $last1=$protein1;
-        $last2=$protein2;
+    if ($score =~ /([0-9.]?)e-(\d+)/) {
+        $factor= ($1 || 1);
+        $magnitude=$2;
+    } elsif ($score eq '0.0') { 
+        $factor = 1;
+        $magnitude=200;
     } else {
-        chop($_);
-        /^(\S+)\s+(\S+)/;
-        print "$1\t$2\t1\t200\n";
+        die "not a valid score: '$score'";
     }
+    #only 
+    if (($last1 ne $protein1) || ($last2 ne $protein2)) {
+        print "$protein1\t$protein2\t$factor\t$magnitude\n";
+    }
+    $last1=$protein1;
+    $last2=$protein2;
 }
 
 warn "Found  total of $total parsed peptides\n";

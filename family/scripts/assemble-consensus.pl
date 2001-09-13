92d8c9f3ec39bfe14887bd255e6c5693f59338b8
Philip Lijnzaad <lijnzaad@sanger.ac.uk>
2001-09-13 15:15:02 +0000

    vastly improved the edits/scoring of annotations. 'd Be nice if swissprot hadn't made it necessary :-(

--- family/scripts/assemble-consensus.pl
+++ family/scripts/assemble-consensus.pl
@@ -31,7 +31,30 @@ if (@ARGV!=3
     die $Usage; 
 }
 
-open (FILEOUT,">$discarded_file") || die "$discarded_file: $!";
+### deletes to be applied to correct some howlers:
+my @deletes = (' FOR\$', 'SIMILAR TO\$', 'SIMILAR TO PROTEIN\$' ); #
+
+### any complete annotation that matches one of the following, gets
+### ticked off completely:
+my @useless_annots = 
+  qw( ^.$  
+      ^\d+$ 
+      .*RIKEN.*FULL.*LENGTH.*ENRICHED.*LIBRARY.*
+    );
+
+### regexp to split the annotations into separate words for scoring:
+my $word_splitter='[\/ \t,:]+';
+
+### words that get scored off; the balance of useful/useless words
+### determines whether they make it through
+my @useless_words =  # and misspellings, that is
+  qw( PROTEIN UNKNOWN FRAGMENT HYPOTHETICAL HYPOTETICAL 
+      NOVEL PUTATIVE PREDICTED UNNAMED UNNMAED
+      PEPTIDE KDA ORF FIS RIKEN CLONE MRNA CDNA
+      KIAA\d+ EST \S+RIK FOR IMAGE:\d+ HSPC\d+
+    );
+
+open (DISCARDED,">$discarded_file") || die "$discarded_file: $!";
 
 my %clusters;
 my $file=$ARGV[0];
@@ -45,111 +68,112 @@ while (<FILE>) {
 }
 close FILE;
 
-my (%swisshash, %swissscore, %tremblhash, %tremblscore);
+my (%descriptions, %scores);
 
-$file = $ARGV[1];
-# make sure we're first reading the SWISSPROT one first ...
+$file = $ARGV[2];
+# make sure we're read the SPTREMBL first, then override with SWISSPROT
+die "$file: expecting 'tr' as part of filename: second one should be trembl" 
+  unless $file =~ /tr/i;
+read_consensus($file, \%descriptions, \%scores);
+
+# now override with SWISSPROT:
+$file=$ARGV[1];
 die "$file: expecting 'sw' as part of filename: first one should be swissprot" 
   unless $file =~ /sw/i;
-open (FILE,$file) || die "$file:$!";
-while (<FILE>)	{
-    /^(\d+)\s+>>>(.*)<<<\s+(\d+)/;
-    $swisshash{$1}=$2;
-    $swissscore{$1}=$3;
-}
-close FILE;
-
-$file=$ARGV[2];
-# ... and sptrembl second:
-die "$file: expecting 'tr' as part of filename: first one should be trembl" 
-  unless $file =~ /tr/i;
-open (FILE,$file) || die "$file:$!";
-while (<FILE>) {
-    /^(\d+)\s+>>>(.*)<<<\s+(\d+)/;
-    $tremblhash{$1}=$2;
-    $tremblscore{$1}=$3;
-}
+read_consensus($file, \%descriptions, \%scores);
 
 my $final_total=0;
-my $total_discarded=0;
-foreach my $cluster_id (sort numeric (keys(%clusters))) {
-    my $members="";
+my $discarded=0;
 
+foreach my $cluster_id (sort numeric (keys(%clusters))) {
     my $annotation="UNKNOWN";
     my $score=0;
 
-    if ($swisshash{$cluster_id}) {
-        $annotation=$swisshash{$cluster_id};
-        $score=$swissscore{$cluster_id};
-        if ($score==0) {
-            $score=1;
-        }
-    } elsif ($tremblhash{$cluster_id}) {
-        $annotation=$tremblhash{$cluster_id};
-        $score=$tremblscore{$cluster_id};
+    if ( $descriptions{$cluster_id}  ) {
+        $annotation=$descriptions{$cluster_id};
+        $score=$scores{$cluster_id};
         if ($score==0) {
             $score=1;
         }
     }
 
-    # Do Some Annotation Checking
+    # apply the deletes:
+    foreach my $re ( @deletes ) { $annotation =~ s/$re//g; }
 
-    my @array=split(" ",$annotation);
-    
-    my $total=$#array+1;
-    my $discarded=0;	
-    
-    foreach my $element (@array) {
-        $_=$element;
-        if (
-            (/^PROTEIN$/) 
-            || (/^FRAGMENT$/) 
-            || (/^\d+$/) 
-            || (/^HSPC\d+/) 
-            || (/^FACTOR$/) 
-            || (/^HYPOTHETICAL$/) 
-            || (/^KIAA\d+/) 
-            || (/^PRECURSOR$/) 
-            || (/^EST$/) 
-            || (/\S+RIK/) 
-            || (/IMAGE:\d+/)
-           )  {              $discarded++;    }
-    }
-    
+    my $useless=0;	
+    my $total= 1;
+
     $_=$annotation;
-    #Global Fixes;
-    if (
-        /CDNA/ 
-        && /FIS/ 
-        && /CLONE/ 
-        && !/WEAKLY SIMILAR/ 
-        && !/MODERATELY SIMILAR/
-       ) {
-        $total=0;
+    # see if the annotation as a whole is useless:
+    if (  grep($annotation =~ /$_/, @useless_annots )   ) {
+        $useless=1000;
+    } else {
+        # word based checking: what is balance of useful/less words:
+        my @words=split(/$word_splitter/,$annotation);
+        $total= int(@words);
+        foreach my $word (@words) {
+            if ( grep( $word =~ /^$_$/, @useless_words ) ) {
+                $useless++;
+            }
+        }
+        $useless += 1 if $annotation =~ /\bKDA\b/;
+        # (because the kiloDaltons come with at least one meaningless number)
     }
-    
-    $_=$annotation;
-    $annotation=~ s/EC (\d+) (\d+) (\d+) (\d+)/EC $1\.$2\.$3\.$4/;
-    $annotation=~ s/EC (\d+) (\d+) (\d+)/EC $1\.$2\.$3\.-/;
-    $annotation=~ s/EC (\d+) (\d+) (\d+)/EC $1\.$2\.-\.-/;
-    $annotation=~ s/(\d+) (\d+) KDA/$1\.$2 KDA/;
-    
-    if (($total-$discarded) <= 0) {
-        print FILEOUT "$cluster_id\t$annotation\t$score\t:$members\n";
+        
+    if ( ($useless >= 1 && $total == 1)
+         || $useless > ($total+1)/2 ) {
+        print DISCARDED "uselessness: $useless/$total: $cluster_id\t$annotation\t$score\n";
+        $discarded++;
         $annotation="UNKNOWN"; 
         $score=0;
     }
-    $total_discarded += $discarded;
 
-    $final_total +=  ($#{$clusters{$cluster_id}}+1);
-    $members=join(":",@{$clusters{$cluster_id}});
-    print "ENSF";
-    printf("%011.0d",$cluster_id+1);
-    print "\t$annotation\t$score\t:$members\n";
+    $_=$annotation;
+    #Apply some fixes to the annotation:
+    s/EC (\d+) (\d+) (\d+) (\d+)/EC $1\.$2\.$3\.$4/;
+    s/EC (\d+) (\d+) (\d+)/EC $1\.$2\.$3\.-/;
+    s/EC (\d+) (\d+) (\d+)/EC $1\.$2\.-\.-/;
+    s/(\d+) (\d+) KDA/$1\.$2 KDA/;
+        
+    #  may well be empty by now; if so, write to discarded
+    if (/^\s*$/) {
+        print DISCARDED "empty: $cluster_id; was: "
+          , $descriptions{$cluster_id},"\n";
+        $discarded++;
+        $annotation="UNKNOWN";
+        $score=0;
+    }
+
+    my @members = @{$clusters{$cluster_id}};
+    $final_total +=  int(@members);
+    printf "ENSF%011.0d\t%s\t%d\t:%d\t%s\n"
+      , $cluster_id+1, $_, $score, join(":",@members);
 }                                       # foreach $cluster_id
+close(DISCARDED);
 
 print STDERR "FINAL TOTAL: $final_total\n";
-print STDERR "discarded: $total_discarded (see $discarded_file)\n";
+print STDERR "discarded: $discarded (see $discarded_file)\n";
 
 sub numeric { $a <=> $b}
 
+
+### read consensus annotations and scores into hashes
+sub read_consensus { 
+    my($file, $deschash, $scorehash)=@_;
+    my (%hash, %score);
+
+    open (FILE,$file) || die "$file:$!";
+
+    while (<FILE>) {
+        my ($id, $desc, $score) = ( /^(\d+)\s+>>>(.*)<<<\s+(\d+)/ );
+        if (0 &&                        # for debugging purposes
+            defined ( $deschash->{$id} ) ) {
+            warn "for $id, replacing '$deschash->{$id}' (score $scorehash->{$id})"
+              ." with '$desc' (score $score)\n";
+        }
+        $deschash->{$1}=$2;
+        $scorehash->{$1}=$3;
+    }
+    close(FILE) || die "$file:$!";
+    undef;
+}

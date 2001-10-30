460c7436dd64705dcc1635e68286875ed20ec582
Philip Lijnzaad <lijnzaad@sanger.ac.uk>
2001-10-30 11:28:42 +0000

    added MAX_ID_LENGTH

--- family/src/markov/markov.h
+++ family/src/markov/markov.h
@@ -1,6 +1,7 @@
+#define MAX_ID_LENGTH 20
 struct hit_struct{
-		    char protein1[16];
-		    char protein2[16];
-		    unsigned char evalue1;
-		    unsigned char evalue2;
-                    };
+  char protein1[MAX_ID_LENGTH +1];
+  char protein2[MAX_ID_LENGTH +1];
+  unsigned char evalue1;
+  unsigned char evalue2;
+};

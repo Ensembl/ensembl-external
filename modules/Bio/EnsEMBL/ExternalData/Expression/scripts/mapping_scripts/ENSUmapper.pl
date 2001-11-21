30243e45ddd0432e0003a08dc86c65dd89bed5d9
Lukasz Huminiecki <lh1@sanger.ac.uk>
2001-11-21 17:09:47 +0000

    as used for the 120 release. takes an hour or two versus one or two days as before. major speed-ups. some changes to criteria by which an overlap with a unigene feature is calculated. still uses a hacky unigene_feature table.
    create table unigene_feature select * from feature where analysis = 11 and score > 300 and hid like "Hs%";
    
    this table is accessed though a hacky method on virtual contig called get_all_unigene_feature
    
    hacky but works, like it or not.

--- modules/Bio/EnsEMBL/ExternalData/Expression/scripts/mapping_scripts/ENSUmapper.pl
+++ modules/Bio/EnsEMBL/ExternalData/Expression/scripts/mapping_scripts/ENSUmapper.pl
@@ -1,76 +1,78 @@
+
+#
+# ensembl120 version
+#
+
+
+
 use strict;
 use Bio::EnsEMBL::DBSQL::Obj;
 use Bio::EnsEMBL::DBSQL::StaticGoldenPathAdaptor;
 
-my $db=Bio::EnsEMBL::DBSQL::Obj->new(-dbname=>"simon_dec12",-user=>"ensro",-host=>"ecs1b");
-
+my $db=Bio::EnsEMBL::DBSQL::DBAdaptor->new(-dbname=>"homo_sapiens_core_120",-user=>"ensro",-host=>"ensrv1"); 
 $db->static_golden_path_type('UCSC');
 
 my $stadaptor = $db->get_StaticGoldenPathAdaptor();
-my $file="chr.dat";
-my $output_file=">ENSUmapper.dat";
-
+my $file="/nfs/acari/lh1/work/sage/for_release/chr.dat";
 open (FH,$file) || die "cant open $file";
-open (OUT,$output_file) || die "cant open $output_file";
-
 while (<FH>){
-    
-    chomp;
+        chomp;
     /^\#/ && next;
     print STDERR "chromosome ",$_,"\n";
     my $contig=$stadaptor->fetch_VirtualContig_by_chr_name("$_");
     print STDERR "fetched vc\n";
-
+    
     my @transcripts=sort {$a->start <=> $b->start}$contig->get_all_VirtualTranscripts_startend();
     print STDERR "sorted transcripts ",$#transcripts,"\n";
 
-    my @features=sort {$a->start <=> $b->start}$contig->get_all_SimilarityFeatures_above_score('unigene.seq',1);
-    print STDERR "sorted features\n";
-    
+    my @unigenes=sort {$a->start <=> $b->start}$contig->get_all_unigene_features();
+    print STDERR "sorted unigenes ", $#unigenes,"\n";
+
     my $transcript_counter;
     my $index_pos=0;
-    my %scores;
-
-  TRANSCRIPT:foreach my $transcript (@transcripts){
+    my $i;
+    TRANSCRIPT:foreach my $transcript (@transcripts)
+      {
+      my $hit = 0;
+      my $top_score = 0;
+      my $top_id; 
       
-      my $counter=0;
-      my $i;
       $transcript_counter++;
-      
-      print STDERR $_,"\t",$transcript->id,"\t",$transcript_counter,"\t",$#transcripts,"\n";
-      
-    FEATURE:for($i=$index_pos;$i<=$#features;$i++){
-	my $seen=0;
-	
-	my $feature=$features[$i];
-	
-	if ($feature->id =~/^Hs/){
-	    
-	    my $id=$feature->id;
-	    my $score=$feature->score; 	
-	    $id=~s/^Hs//;
-	    $id=~s/\.//;
-	    
-	    if ($feature->start>=$transcript->start && $feature->end<=$transcript->end){
-		$counter++;
-		$seen=1;		    
-		$scores{$score} = $id;
-	    }
-	    if ($counter>0 && $seen==0)
-	    {
-		my @keys = (sort {$b <=> $a} keys %scores);
-		print OUT $transcript->id,"\t", $scores{$keys[0]}, "\t", $keys[0],"\n";
-		%scores = ();
-		next TRANSCRIPT; 
-	    }
-	}    
-    }
-  }
+
+      print STDERR $_,"\t",$transcript->gene->dbID,"\t",$transcript_counter,"\t",$#transcripts,"\n";  # where are we now? report to SDTERR
+
+my $u = 0;      
+    UNIGENE:for($i = $index_pos; $i <= $#unigenes; $i++)
+        {
+	  $u++;
+        my $unigene   = $unigenes[$i];
+        my $id        = $unigene->id;
+	my $score     = $unigene->score; 	
+        if (($unigene->start >= $transcript->start && $unigene->end <= $transcript->end))
+# || ($unigene->end >= $transcript->start && $unigene->end <= $transcript->end))
+	        {
+                if ($score > $top_score)   
+		   {
+         	   $top_score = $score;
+                   $top_id    = $id;
+		   $hit       = 1;
+      		   }
+	        }
+        if ($unigene->start > $transcript->end)
+	   {
+	   $index_pos = $i - 1;     
+	   if ($hit == 1)
+       	        {
+                $top_id =~ s/Hs\.//;
+		print $transcript->gene->dbID, "\t", $top_id, "\t", $top_score, "\n"; 
+                print STDERR "I have looked through ", $u, "unigenes for you", "\n"; 
+                }
+           next TRANSCRIPT;           
+	   }
+        }
+        
+    
+        
+      }
 }
-
-print STDERR "I have finished!!!!!!!!!!!!!!!!!!!!!\n";
-
-
-
-
-
+print STDERR "I have finished!\n";

0682e26f6f0dcc05d246ac8f04a83a0c198f312d
Elia Stupka <elia@sanger.ac.uk>
2002-06-13 17:16:23 +0000

    Committing improved family schema to the main trunk

--- sql/family.sql
+++ sql/family.sql
@@ -3,31 +3,51 @@
 # tables for Anton Enrights protein family clustering. 
 
 CREATE TABLE family (
-   internal_id   int(10) NOT NULL,
-   id            varchar(40) NOT NULL, ##  e.g. ENSF0000012345
-   description   varchar(255) NOT NULL,
-   release       varchar(10) NOT NULL,
+   family_id    int(10) NOT NULL,
+   stable_id  varchar(40) NOT NULL, ##  e.g. ENSF0000012345
+   description  varchar(255) NOT NULL,
+   release      varchar(10) NOT NULL,
    annotation_confidence_score double, 
 
-   num_ens_pepts int(10) DEFAULT '0' NOT NULL,
-    
-   PRIMARY KEY(internal_id), 
-   UNIQUE KEY(id)
+   PRIMARY KEY(family_id), 
+   UNIQUE KEY(stable_id),
+   KEY(description),
+   KEY(release)
 );
+
+CREATE TABLE family_totals (
+   family_id int(10) NOT NULL, #foreign key to family table
+   external_db_id int(10) NOT NULL, #foreign key to external_db table 
+   members_total int(10) NOT NULL,
+
+   PRIMARY KEY (family_id,external_db_id),
+   KEY (members_total)
+);
+
 
 CREATE TABLE alignments (
-  family              int(10) NOT NULL,
-  alignment           mediumtext NOT NULL,
-  PRIMARY KEY(FAMILY)
+  family_id  int(10) NOT NULL, #foreign key to family table
+  alignment  mediumtext NOT NULL,
+  PRIMARY KEY(family_id)
 );
 
+CREATE TABLE external_db (
+  external_db_id int(10) NOT NULL auto_increment,
+  name varchar(40) NOT NULL,
+
+  PRIMARY KEY(external_db_id),
+  UNIQUE KEY(name)
+);
+
+
 CREATE TABLE family_members (
-  family	      int(10) NOT NULL,
-  db_name             varchar(12) NOT NULL, 
-                                 ## currently ENSEMBLPEP, SPTREMBL, SWISSPROT
-  db_id               varchar(40) NOT NULL, ##  e.g., ENSP000001234 or P12345
-    PRIMARY KEY(family, db_name, db_id),
-    UNIQUE KEY(db_name, db_id)
+  family_id	      int(10) NOT NULL,
+  external_db_id        int(10) NOT NULL, #foreign key to family_db table 
+  external_member_id  varchar(40) NOT NULL, ##  e.g., ENSP000001234
+  PRIMARY KEY(family_id, external_db_id, external_member_id),
+  UNIQUE KEY(external_db_id, external_member_id),
+  KEY(external_db_id),
+  KEY(family_id,external_db_id)
 );
 
 CREATE TABLE cumulative_distrib (

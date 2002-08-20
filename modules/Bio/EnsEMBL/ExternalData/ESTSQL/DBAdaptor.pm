5177d4ccecedb9a40b360834ae573f8b4d0c21fc
Graham McVicker <mcvicker@sanger.ac.uk>
2002-08-20 14:52:02 +0000

    Restructured DBAdaptor to conform to main trunk - now only provides a few adaptors. Removed EstAdaptor.  The core's DnaAlignFeatureAdaptor should be used instead

--- modules/Bio/EnsEMBL/ExternalData/ESTSQL/DBAdaptor.pm
+++ modules/Bio/EnsEMBL/ExternalData/ESTSQL/DBAdaptor.pm
@@ -15,7 +15,14 @@
 
 =head1 DESCRIPTION
 
+This is a DBAdaptor for the EST database.  The EST database only contains
+two tables - an analysis table and a dna_feature_table of DnaDnaAlignFeatures.
+This database is not standalone because the the features which are pulled out
+make reference the contigs and the assembly so that they can be
+converted to the assembly cooridinate system.  These calls are therefore 
+forwarded to the core database adaptor which may be attached to this adaptor.
 
+This may be cleaned up a bit with the advent of a database registry object.
 
 =head1 FEEDBACK
 
@@ -34,170 +41,56 @@ The rest of the documentation details each of the object methods. Internal metho
 
 package Bio::EnsEMBL::ExternalData::ESTSQL::DBAdaptor;
 
-use Bio::EnsEMBL::ExternalData::ESTSQL::EstAdaptor;
-use Bio::EnsEMBL::DB::ExternalFeatureFactoryI;
-use Bio::EnsEMBL::Root;
+use Bio::EnsEMBL::DBSQL::DBConnection;
 use DBI;
 
 use vars qw(@ISA);
 
-@ISA = qw(Bio::EnsEMBL::Root Bio::EnsEMBL::DB::ExternalFeatureFactoryI);
+@ISA = qw(Bio::EnsEMBL::DBSQL::DBConnection);
+
+#new inherited from Bio::EnsEMBL::DBConnection
 
 
+sub get_DnaAlignFeatureAdaptor {
+  my $self = shift;
 
-sub new {
-    my($class,@args) = @_;
-    my $self;
-    $self = {};
-    bless $self, $class;
-    
-    my ($db,$host,$port,$driver,$user,$password) =
-	$self->_rearrange([qw(DBNAME
-			      HOST
-			      PORT
-			      DRIVER
-			      USER
-			      PASS
-			      )],@args);
-    
-    $db   || $self->throw("Database object must have a database name");
-    $user || $self->throw("Database object must have a user");
-    
-    if( ! $driver ) {
-	$driver = 'mysql';
-    }
-    if( ! $host ) {
-	$host = 'localhost';
-    }
+  return $self->_get_adaptor("Bio::EnsEMBL::DBSQL::DnaAlignFeatureAdaptor");
+}
 
-    if (! $port ) {
-	$port = 3306;
-    }
-    
-    my $dsn = "DBI:$driver:database=$db;host=$host;port=$port";
-    my $dbh = DBI->connect("$dsn","$user",$password);
-    
-    $dbh || $self->throw("Could not connect to database $db user $user using [$dsn] as a locator");
-    
-    $self->_db_handle($dbh);
-    
-    return $self; # success - we hope!
+sub get_AnalysisAdaptor {
+  my $self = shift;
+
+  return $self->_get_adaptor("Bio::EnsEMBL::DBSQL::AnalysisAdaptor");
 }
 
 
-=head2 get_EstAdaptor
-
- Title   : get_EstAdaptor
- Usage   :
- Function:
- Example :
- Returns : 
- Args    :
-
-
-=cut
-
-
-sub get_EstAdaptor {
+#
+# Forward requests for the RawContigAdaptor to the core database
+#
+sub get_RawContigAdaptor {
   my $self = shift;
 
-  if( ! defined $self->{_EstAdaptor} ) {
-    $self->{_EstAdaptor} = Bio::EnsEMBL::ExternalData::ESTSQL::EstAdaptor->new( $self );
+  my $core = $self->core_DBAdaptor();
+
+  unless(defined $core) {
+    $self->throw("No core database is attached to the ESTDatabase.  The EST "
+		 . "database does not contain any contig information\n");
   }
-  return $self->{_EstAdaptor};
+  #if the core database is available use its raw contigs
+  return $core->get_RawContigAdaptor();
 }
 
-=head2 get_Ensembl_SeqFeatures_contig
+sub core_DBAdaptor {
+  my ($self, $arg) = @_;
 
- Title   : get_Ensembl_SeqFeatures_contig
- Usage   :
- Function:
- Example :
- Returns : 
- Args    :
-
-
-=cut
-
-sub get_Ensembl_SeqFeatures_contig{
-   my ($self,@args) = @_;
-
-   return $self->get_EstAdaptor->get_Ensembl_SeqFeatures_contig(@args);
-
-}
-
-=head2 get_Ensembl_SeqFeatures_clone
-
- Title   : get_Ensembl_SeqFeatures_clone (Abstract)
- Usage   :
- Function:
- Example :
- Returns : 
- Args    :
-
-
-=cut
-
-sub get_Ensembl_SeqFeatures_clone{
-   my ($self,@args) = @_;
-   
-   return $self->get_EstAdaptor->get_Ensembl_SeqFeatures_clone(@args);
-
-}
-
-=head2 get_Ensembl_Genes_clone
-
- Title   : get_Ensembl_Genes_clone
- Function: returns Gene objects in clone coordinates from a gene id
- Returns : An array of Gene objects
- Args    : clone id
-
-=cut
-
-sub get_Ensembl_Genes_clone {
-    my $self = @_;
-
-    $self->throw("get_Ensembl_Genes_clone is not valid for the est database");
-}
-
-=head2 get_SeqFeature_by_id
-
- Title   : get_SeqFeature_by_id (Abstract)
- Usage   : 
- Function: Return SeqFeature object for any valid unique id  
- Example :
- Returns : 
- Args    : id as determined by the External Database
-
-
-=cut
-
-       
-sub get_SeqFeature_by_id {
-   my ($self,$id) = @_;
-
-   return $self->get_EstAdaptor->get_SeqFeature_by_id($id);
-
-}
-
-sub prepare {
-  my $self = shift;
-  my $query = shift;
-
-  return $self->_db_handle->prepare( $query );
-}
-
-
-sub _db_handle {
-  my $self = shift;
-  my $handle = shift;
-  
-  if( defined $handle ) {
-    $self->{_db_handle} = $handle;
+  if(defined $arg) {
+    $self->{'_core_dbadaptor'} = $arg;
   }
 
-  return $self->{_db_handle};
+  return $self->{'_core_dbadaptor'};
 }
+
+
 
 1;
 

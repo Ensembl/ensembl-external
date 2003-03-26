ed5ef0adcfdff650efd61546eb15268d4bfbbe6a
Abel Ureta-Vidal <abel@sanger.ac.uk>
2003-03-26 17:17:59 +0000

    clean the code...

--- family/scripts/parse_mcl.pl
+++ family/scripts/parse_mcl.pl
@@ -1,45 +1,57 @@
-#!/usr/local/bin/perl
+#!/usr/local/ensembl/bin/perl -w
 # $Id$
 
 # Parse MCL output (numbers) back into real clusters (with protein names)
 
 use strict;
 
-my $Usage = "Usage: $0 file.mcl file.index > file.clusters";
+my $usage = "
+Usage: $0 mcl_file index_file > mcl.clusters
+\n";
 
-die $Usage unless @ARGV == 2;
+die $usage unless (scalar @ARGV == 2);
 
-my $file=$ARGV[0];
+my ($mcl_file,$index_file) = @ARGV;
 
-open (FILE, $file) || die "$file: $!";
-my $i=0;
-my @cluster=undef;
-my %hash = undef;
+my @clusters;
+my %members_index;
+my $headers_off = 0;
+my $one_line_members = "";
 
-while (<FILE>) {
-    if ((!/begin/) && (!/\)/) && (!/mcl/) &&  (!/dimensions/) && (!/\(/) )  {
-        chomp;
-        $cluster[$i]=join(" ",$cluster[$i],"$_");
-        
-	if (/\$/) {
-            $i++;
-        }
-    }
+open MCL, $mcl_file ||
+  die "$mcl_file: $!";
+
+while (<MCL>) {
+  if (/^begin$/) {
+    $headers_off = 1;
+    next;
+  }
+  next unless ($headers_off);
+  last if (/^\)$/);
+  chomp;
+  $one_line_members .= $_;
+  if (/\$/) {
+    push @clusters, $one_line_members;
+    $one_line_members = "";
+  }
 }
-close(FILE) || die "$file: $!";
 
-$file=$ARGV[1];
-open (FILE,$file) || die "$file: $!";
-while (<FILE>) {
+close MCL ||
+  die "$mcl_file: $!";
+
+open INDEX, $index_file ||
+  die "$index_file: $!";
+
+while (<INDEX>) {
     /^(\S+)\s+(\S+)/;
-    $hash{$1}=$2;
+    $members_index{$1}=$2;
 }
-close(FILE)|| die "$file: $!";
+close(INDEX)|| die "$index_file: $!";
 
-for (my $j=0;$j<$i;$j++)	{
-    my @array=split(" ",$cluster[$j]);
-    
-    for (my $p=1;$p< $#array; $p++) {
-        print "$array[0]\t$hash{$array[$p]}\n";
-    }
+foreach my $cluster (@clusters) {
+  my ($cluster_index, @cluster_members) = split /\s+/,$cluster;
+  foreach my $member (@cluster_members) {
+    last if ($member =~ /^\$$/);
+    print "$cluster_index\t$members_index{$member}\n"
+  }
 }

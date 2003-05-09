5a8b9217088e187d9d5b5272a3b0dd5b4652c809
Abel Ureta-Vidal <abel@sanger.ac.uk>
2003-05-09 10:01:48 +0000

    now able to run on blades machines with fastafetch, and gzip the output

--- family/scripts/LaunchBlast.pl
+++ family/scripts/LaunchBlast.pl
@@ -6,8 +6,16 @@ use File::Basename;
 
 my ($idqy,$fastadb,$fastaindex,$dir);
 
-my $blast_executable = "/usr/local/ensembl/bin/blastall-2.2.1";
-my $fastafetch_executable = "/nfs/acari/abel/bin/fastafetch";
+my $blast_executable = "/usr/local/ensembl/bin/blastall-2.2.1"; 
+my $fastafetch_executable = "/nfs/acari/abel/bin/alpha-dec-osf4.0/fastafetch";
+
+if (-e "/proc/version") {
+  # it is a linux machine
+  $fastafetch_executable = "/nfs/acari/abel/bin/i386/fastafetch";
+}
+
+print STDERR "fastafetch_executable: ",$fastafetch_executable,"\n";
+
 my $tribe_parse_executable = "/nfs/acari/abel/bin/tribe-parse";
 
 GetOptions('idqy=s' => \$idqy,
@@ -31,7 +39,6 @@ unless(system("$fastafetch_executable $fastadb $fastaindex $idqy > $qy_file") ==
 } 
 
 my $status = system("$blast_executable -d $fastadb -i $qy_file -p blastp -e 0.00001 > $blast_file");
-#unless ($status == 0 || $status == 4096 || $status == 5888) {
 unless ($status == 0) {
   unlink glob("/tmp/*$rand*");
   die "error in $blast_executable, $!\n";
@@ -42,7 +49,7 @@ unless (system("$tribe_parse_executable $blast_file > $blast_tribe_file") == 0) 
 }
 
 my $final_file = $dir."/".basename($idqy).".blast_tribe";
-unless (system("cp $blast_tribe_file $final_file") == 0) {
+unless (system("gzip -c $blast_tribe_file > $final_file.gz") == 0) {
   unlink glob("/tmp/*$rand*");
   die "error in cp $blast_tribe_file, $!\n";
 }

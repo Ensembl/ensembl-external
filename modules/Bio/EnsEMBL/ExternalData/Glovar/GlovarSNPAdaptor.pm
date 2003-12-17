e6ab13d4be61f5c98bf90d8982a0860dc5fc8f43
Patrick Meidl <pm2@sanger.ac.uk>
2003-12-17 12:21:22 +0000

    added fetch_SNP_by_id(), experimental version

--- modules/Bio/EnsEMBL/ExternalData/Glovar/GlovarSNPAdaptor.pm
+++ modules/Bio/EnsEMBL/ExternalData/Glovar/GlovarSNPAdaptor.pm
@@ -41,7 +41,8 @@ loaded using a separate system).
 
 =head1 APPENDIX
 
-The rest of the documentation details each of the object methods. Internal methods are usually preceded with a _
+The rest of the documentation details each of the object methods. Internal
+methods are usually preceded with a _
 
 =cut
 
@@ -113,44 +114,44 @@ sub fetch_Light_Variations_by_chr_start_end  {
     my $ass_version  = $slice->assembly_version();
 
     my $q1 = qq(
-        SELECT                                                                                                                                  
-                mapped_snp.position + seq_seq_map.START_COORDINATE -1                                                                       
-                                                as chr_start,                                                                                
-                mapped_snp.id_snp               as internal_id,                                                                              
-                seq_seq_map.contig_orientation  as chr_strand,                                                                               
-                snp_name.snp_name               as id_refsnp,                                                                                
-                'glovar'                        as source,                                                                                   
-                'snp'                           as snpclass,                                                                                 
-                svd.description                 as type,                                                                                     
-                snp.is_private                  as private,                                                                                     
-                snpf.allele_txt(snp.id_snp)     as alleles,                                                                                  
-                scd.description                 as validated                                                                                 
-        FROM    chrom_seq,                                                                                                                   
-                seq_seq_map,                                                                                                                 
-                snp_sequence,                                                                                                                
-                mapped_snp,                                                                                                                  
-                snp_name,                                                                                                                    
-                snp,                                                                                                                         
-                snpvartypedict svd,                                                                                                          
-                snp_confirmation_dict scd,                                                                                                   
-                database_dict                                                                                                                
-        WHERE   chrom_seq.DATABASE_SEQNAME= '$slice_chr'                                                                                             
-        AND     chrom_seq.ID_CHROMSEQ = seq_seq_map.ID_CHROMSEQ                                                                              
-        AND     snp_sequence.ID_SEQUENCE = seq_seq_map.SUB_SEQUENCE                                                                          
-        AND     mapped_snp.ID_SEQUENCE = snp_sequence.ID_SEQUENCE                                                                            
-        AND     snp_name.id_snp = mapped_snp.id_snp                                                                                          
-        AND     snp.id_snp = snp_name.id_snp                                                                                                 
-        AND     snp.var_type = svd.id_dict                                                                                                   
-        AND     snp.is_confirmed = scd.id_dict                                                                                               
-        AND     snp_name.snp_name_type = 1                                                                                                   
-        AND     chrom_seq.DATABASE_SOURCE = database_dict.ID_DICT                                                                            
-        AND     database_dict.DATABASE_NAME = '$ass_name'                                                                                         
-        AND     database_dict.DATABASE_VERSION = '$ass_version'                                                                                        
-        AND     (mapped_snp.position + seq_seq_map.START_COORDINATE -1) 
-        BETWEEN                                                              
-                '$slice_start' AND '$slice_end'                                                                                                              
-        ORDER BY 
-                chr_start                                                                                                                   
+        SELECT
+                mapped_snp.position + seq_seq_map.START_COORDINATE -1
+                                                as chr_start,
+                mapped_snp.id_snp               as internal_id,
+                seq_seq_map.contig_orientation  as chr_strand,
+                snp_name.snp_name               as id_refsnp,
+                'glovar'                        as source,
+                'snp'                           as snpclass,
+                svd.description                 as type,
+                snp.is_private                  as private,
+                snpf.allele_txt(snp.id_snp)     as alleles,
+                scd.description                 as validated
+        FROM    chrom_seq,
+                seq_seq_map,
+                snp_sequence,
+                mapped_snp,
+                snp_name,
+                snp,
+                snpvartypedict svd,
+                snp_confirmation_dict scd,
+                database_dict
+        WHERE   chrom_seq.DATABASE_SEQNAME= '$slice_chr'
+        AND     chrom_seq.ID_CHROMSEQ = seq_seq_map.ID_CHROMSEQ
+        AND     snp_sequence.ID_SEQUENCE = seq_seq_map.SUB_SEQUENCE
+        AND     mapped_snp.ID_SEQUENCE = snp_sequence.ID_SEQUENCE
+        AND     snp_name.id_snp = mapped_snp.id_snp
+        AND     snp.id_snp = snp_name.id_snp
+        AND     snp.var_type = svd.id_dict
+        AND     snp.is_confirmed = scd.id_dict
+        AND     snp_name.snp_name_type = 1
+        AND     chrom_seq.DATABASE_SOURCE = database_dict.ID_DICT
+        AND     database_dict.DATABASE_NAME = '$ass_name'
+        AND     database_dict.DATABASE_VERSION = '$ass_version'
+        AND     (mapped_snp.position + seq_seq_map.START_COORDINATE -1)
+        BETWEEN
+                '$slice_start' AND '$slice_end'
+        ORDER BY
+                chr_start
     );
 
       my $q2 = qq(SELECT ms.id_sts,
@@ -285,9 +286,156 @@ sub fetch_Variations_by_chr_start_end  {
     return(\@vars);
 }                                       
 
-sub fetch_Variation_by_id  {
+=head2 fetch_SNP_by_id
+
+  Arg[1]      : String - Variation ID
+  Example     : my $variation = $glovar_adaptor->fetch_SNP_by_id($id);
+  Description : retrieve variations from Glovar by ID
+  Return type : List of Bio::EnsEMBL::SNP
+
+=cut
+
+sub fetch_SNP_by_id  {
     my ($self, $id) = @_;
-    return(1);
+    
+    ## get assembly name and version from DNA db
+    my $ass_name = $self->db->dnadb->assembly_name;
+    my $ass_version = $self->db->dnadb->assembly_version;
+    
+    my $q1 = qq(
+        SELECT
+                snp_summary.ID_SNP              as INTERNAL_ID,
+                snp_summary.DEFAULT_NAME        as ID_DEFAULT,
+                mapped_snp.POSITION + seq_seq_map.START_COORDINATE -1
+                                                as CHR_START,
+                mapped_snp.END_POSITION         as CHR_END,
+                seq_seq_map.CONTIG_ORIENTATION  as CHR_STRAND,
+                'Glovar'                        as SOURCE,
+                'SNP'                           as SNPCLASS,
+                scd.DESCRIPTION                 as VALIDATED,
+                snp_sequence.DATABASE_SEQNNAME      as SEQNAME,
+                snp_sequence.DATABASE_SEQVERSION    as SEQVERSION,
+                snp_sequence.CHROMOSOME         as CHR,
+                snp_summary.ALLELES             as ALLELES,
+                snp_summary.IS_PRIVATE          as PRIVATE
+        FROM    chrom_seq,
+                database_dict,
+                seq_seq_map,
+                snp_sequence,
+                mapped_snp,
+                snp_summary,
+                snp_confirmation_dict scd
+        WHERE   snp_summary.DEFAULT_NAME = '$id'
+        AND     database_dict.DATABASE_NAME = '$ass_name'
+        AND     database_dict.DATABASE_VERSION = '$ass_version'
+        AND     database_dict.ID_DICT = chrom_seq.DATABASE_SOURCE
+        AND     chrom_seq.ID_CHROMSEQ = seq_seq_map.ID_CHROMSEQ
+        AND     seq_seq_map.SUB_SEQUENCE = snp_sequence.ID_SEQUENCE
+        AND     snp_sequence.ID_SEQUENCE = mapped_snp.ID_SEQUENCE
+        AND     mapped_snp.ID_SNP = snp_summary.ID_SNP
+        AND     snp_summary.CONFIRMATION_STATUS = scd.ID_DICT
+    );
+
+    my @vars = ();
+    my $sth;
+
+    eval {
+        $sth = $self->prepare($q1);
+        $sth->execute();
+    }; 
+    if ($@){
+        warn("ERROR: SQL failed in GlovarAdaptor->fetch_SNP_by_id()!
+            \n$@");
+        return;
+    }
+
+    my $row = $sth->fetchrow_hashref();
+    if (!(keys %{$row}) || $row->{'PRIVATE'}){
+        return;
+    }
+
+    my $snp = Bio::EnsEMBL::SNP->new;
+    
+    my $acc_version = '';
+    $acc_version = $row->{'SEQNAME'} if $row->{'SEQNAME'};
+    $acc_version .= "." . $row->{'SEQVERSION'} if $row->{'SEQVERSION'};
+
+    $snp->dbID($row->{'INTERNAL_ID'});
+    $snp->seqname($acc_version);
+    $snp->start($row->{'CHR_START'});
+    $snp->end($row->{'CHR_END'});
+    $snp->strand($row->{'CHR_STRAND'});
+    $snp->original_strand($row->{'CHR_STRAND'});
+    
+    $snp->source_tag($row->{'SOURCE'});
+    $snp->snpclass($row->{'SNPCLASS'});
+    $snp->raw_status($row->{'VALIDATED'});
+
+    $snp->alleles($row->{'ALLELES'});
+
+    my $slice = $self->db->dnadb->get_SliceAdaptor->fetch_by_chr_start_end(
+       $row->{'CHR'},
+       $row->{'CHR_START'} - 20,
+       $row->{'CHR_START'},
+    );
+    $snp->upStreamSeq($slice->seq);
+    
+    if (0) {
+        my $seq_adaptor = $self->db->dnadb->get_SequenceAdaptor;
+        my $seq5 = $seq_adaptor->fetch_by_assembly_location(
+           $row->{'CHR_START'} - 20,
+           $row->{'CHR_START'},
+           $row->{'CHR_STRAND'},
+           $row->{'CHR'},
+           $self->db->dnadb->get_MetaContainer->get_default_assembly
+        );
+        $snp->upStreamSeq($seq5);
+    }
+    #$snp->dnStreamSeq($seq3);
+
+    ## not available
+    #$snp->score($mapweight); 
+    #$snp->het($het);
+    #$snp->hetse($hetse);
+    
+    #DBLink
+    my $q2 = qq(
+        SELECT
+                snp_name.SNP_NAME               as NAME,
+                snpnametypedict.DESCRIPTION     as TYPE
+        FROM    
+                snp_name,
+                snpnametypedict
+        WHERE   snp_name.ID_SNP = ?
+        AND     snp_name.SNP_NAME_TYPE = snpnametypedict.ID_DICT
+    );
+    my $sth2;
+    eval {
+        $sth2 = $self->prepare($q2);
+        $sth2->execute($row->{'INTERNAL_ID'});
+    }; 
+    if ($@){
+        warn("ERROR: SQL failed in GlovarAdaptor->fetch_SNP_by_id()!
+            \n$@");
+        return;
+    }
+
+    while (my $xref = $sth2->fetchrow_hashref()) {
+        my $link = new Bio::Annotation::DBLink;
+        $link->database($xref->{'TYPE'});
+        $link->primary_id($xref->{'NAME'});
+        $snp->add_DBLink($link);
+    }
+    
+    ## sanity check for more result rows
+    while (my $row = $sth->fetchrow_hashref()) {
+        if (keys %{$row}) {
+            warn "Glovar returned more than one result row!";
+            last;
+        }
+    }
+    
+    return $snp;
 }
 
 
@@ -296,5 +444,5 @@ sub track_name {
     return("GlovarSNP");
 }
 
-
 1;
+

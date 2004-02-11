97fe3706e112067823aca9d9905424cbeac477be
Web Admin <w3adm@sanger.ac.uk>
2004-02-11 10:30:23 +0000

    fixed inability to tell BED from generic

--- modules/Data/Bio/Text/Feature/generic.pm
+++ modules/Data/Bio/Text/Feature/generic.pm
@@ -4,9 +4,9 @@ use Data::Bio::Text::Feature;
 use vars qw(@ISA);
 @ISA = qw(Data::Bio::Text::Feature);
 
-sub seqname   	{  return $_[0]->{'__raw__'}[0]; }
-sub rawstart 	{  return $_[0]->{'__raw__'}[1]; }
-sub rawend 		{  return $_[0]->{'__raw__'}[2]; }
-sub id       	{  return $_[0]->{'__raw__'}[3]; }
+sub seqname   	{  return $_[0]->{'__raw__'}[1]; }
+sub rawstart 	{  return $_[0]->{'__raw__'}[2]; }
+sub rawend 	{  return $_[0]->{'__raw__'}[3]; }
+sub id       	{  return $_[0]->{'__raw__'}[4]; }
 
 1;

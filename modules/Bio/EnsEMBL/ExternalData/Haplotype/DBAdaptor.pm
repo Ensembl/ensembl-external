3424568b7ddfcb962e360d799e0b37c277bbaf5e
Web Admin <w3adm@sanger.ac.uk>
2004-02-17 11:05:35 +0000

    *** empty log message ***

---
+++ modules/Bio/EnsEMBL/ExternalData/Haplotype/DBAdaptor.pm
@@ -0,0 +1,28 @@
+use strict;
+
+package Bio::EnsEMBL::ExternalData::Haplotype::DBAdaptor;
+
+use vars qw(@ISA);
+use Bio::EnsEMBL::DBSQL::DBConnection;
+
+@ISA = qw(Bio::EnsEMBL::DBSQL::DBConnection);
+
+=head2 get_HaplotypeAdaptor
+
+  Arg [1]    : none
+  Example    : $haplotype_adaptor = new Bio::EnsEMBL::HaplotypeAdaptor;
+  Description: Retreives a haplotype adaptor
+  Returntype : none
+  Exceptions : none
+  Caller     : EnsWeb, general
+
+=cut
+
+sub get_HaplotypeAdaptor {
+  my $self = shift;
+
+  return $self->_get_adaptor(
+		'Bio::EnsEMBL::ExternalData::Haplotype::HaplotypeAdaptor');
+}
+
+1;

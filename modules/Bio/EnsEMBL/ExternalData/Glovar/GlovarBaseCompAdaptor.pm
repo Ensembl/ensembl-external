e74fa8b296835b58f0d0e97113b842763110e23c
Patrick Meidl <pm2@sanger.ac.uk>
2004-02-22 10:51:44 +0000

    pm2 rewrite of base composition track

--- modules/Bio/EnsEMBL/ExternalData/Glovar/GlovarBaseCompAdaptor.pm
+++ modules/Bio/EnsEMBL/ExternalData/Glovar/GlovarBaseCompAdaptor.pm
@@ -49,15 +49,11 @@ methods are usually preceded with a _
 =cut
 
 package Bio::EnsEMBL::ExternalData::Glovar::GlovarBaseCompAdaptor;
-use vars qw(@ISA);
-use strict;
-use Data::Dumper;
 
-use Bio::EnsEMBL::DBSQL::BaseAdaptor;
-use Bio::EnsEMBL::External::ExternalFeatureAdaptor;
-use Bio::EnsEMBL::SeqFeature;
-use Bio::EnsEMBL::GlovarBaseComp;
-use Bio::Annotation::DBLink;
+use strict;
+use vars qw(@ISA);
+
+use Bio::EnsEMBL::ExternalData::Glovar::BaseComposition;
 use Bio::EnsEMBL::ExternalData::Glovar::GlovarAdaptor;
 use Bio::EnsEMBL::Utils::Eprof qw(eprof_start eprof_end eprof_dump);
 
@@ -105,84 +101,124 @@ sub fetch_all_by_Slice {
 =cut
 
 sub fetch_Light_Base_Comp_by_chr_start_end {
-  my ($self,$slice) = @_;
-  my $slice_chr    = $slice->chr_name();
-  my $slice_start  = $slice->chr_start();
-  my $slice_end    = $slice->chr_end();
-  my $slice_strand = $slice->strand();
-  my $ass_name     = $slice->assembly_name();
-  my $ass_version  = $slice->assembly_version();
+    my ($self,$slice) = @_;
+    my $slice_chr    = $slice->chr_name();
+    my $slice_start  = $slice->chr_start();
+    my $slice_end    = $slice->chr_end();
+    my $slice_strand = $slice->strand();
+    my $ass_name     = $slice->assembly_name();
+    my $ass_version  = $slice->assembly_version();
 
-  my $q = qq(SELECT 	(rp.position + ssm.start_coordinate -1) as position,
-	rp.genomic_base as genomic_base,
-	COUNT(decode (sp.base,'A',1
-			     ,'G',NULL
-			     ,'T',NULL
-			     ,'C',NULL)) as A,
-	COUNT(decode (sp.base,'A',NULL
-			     ,'G',1
-			     ,'T',NULL
-			     ,'C',NULL)) as G,
-	COUNT(decode (sp.base,'A',NULL
-			     ,'G',NULL
-			     ,'T',NULL
-			     ,'C',1)) as C,
-	COUNT(decode (sp.base,'A',NULL
-			     ,'G',NULL
-			     ,'T',1
-			     ,'C',NULL)) as T
-FROM	chrom_seq cs,
-	seq_seq_map ssm,
-	snp_sequence ss,
-	reference_position rp,
-	sequence_position sp
-WHERE	cs.database_seqname = '$slice_chr'
-AND     cs.is_current = 1
-AND	cs.id_chromseq = ssm.id_chromseq
-AND	ssm.sub_sequence = ss.id_sequence
-AND	ss.id_sequence = rp.id_sequence
-AND	rp.id_sequence = sp.repo_id_sequence
-AND	sp.repo_position = rp.position
-AND     sp.is_nqs = 1
-AND	rp.position
-	BETWEEN	($slice_start - ssm.start_coordinate + 1)
-	AND	($slice_end - ssm.start_coordinate + 1)
-GROUP BY (rp.position + ssm.start_coordinate),
-	  rp.genomic_base
-ORDER by position);
+    &eprof_start('glovar_basecomp');
 
-  my $sth;
-  eval {
+    ## NOTE:
+    ## all code here assumes that ssm.contig_orientation is always 1!
+
+    my $q = qq(
+        SELECT     
+                (rp.position + ssm.start_coordinate -1) as position,
+                rp.genomic_base as genomic_base,
+                COUNT(decode (sp.base,'A',1
+                        ,'G',NULL
+                        ,'T',NULL
+                        ,'C',NULL)) as A,
+                COUNT(decode (sp.base,'A',NULL
+                        ,'G',1
+                        ,'T',NULL
+                        ,'C',NULL)) as G,
+                COUNT(decode (sp.base,'A',NULL
+                        ,'G',NULL
+                        ,'T',NULL
+                        ,'C',1)) as C,
+                COUNT(decode (sp.base,'A',NULL
+                        ,'G',NULL
+                        ,'T',1
+                        ,'C',NULL)) as T,
+                COUNT(decode (ed.id_dict,0,1
+                        ,1,NULL
+                        ,2,NULL
+                        ,3,NULL)) as E0,
+                COUNT(decode (ed.id_dict,0,NULL
+                        ,1,1
+                        ,2,NULL
+                        ,3,NULL)) as E1,
+                COUNT(decode (ed.id_dict,0,NULL
+                        ,1,NULL
+                        ,2,1
+                        ,3,NULL)) as E2,
+                COUNT(decode (ed.id_dict,0,NULL
+                        ,1,NULL
+                        ,2,NULL
+                        ,3,1)) as E3
+        FROM    chrom_seq cs,
+                seq_seq_map ssm,
+                snp_sequence ss,
+                reference_position rp,
+                sequence_position sp,
+                snp_read sr,
+                individual i,
+                ethnicitydict ed
+        WHERE   cs.database_seqname = '$slice_chr'
+        AND     cs.is_current = 1
+        AND     cs.id_chromseq = ssm.id_chromseq
+        AND     ssm.sub_sequence = ss.id_sequence
+        AND     ss.id_sequence = rp.id_sequence
+        AND     rp.id_sequence = sp.repo_id_sequence
+        AND     sp.repo_position = rp.position
+        AND     sp.is_nqs = 1
+        AND     sp.seq_content = sr.id_read
+        AND     sr.dna_content = i.id_ind
+        AND     i.ethnicity = ed.id_dict
+        AND     rp.position
+                BETWEEN
+                ($slice_start - ssm.start_coordinate + 1)
+                AND
+                ($slice_end - ssm.start_coordinate + 1)
+        GROUP BY
+                (rp.position + ssm.start_coordinate),
+                rp.genomic_base
+        ORDER by position
+    );
+
+    my $sth;
+    eval {
         $sth = $self->prepare($q);
         $sth->execute();
-  };
-  if ($@){
-    warn("ERROR: SQL failed in GlovarAdaptor->fetch_Light_Base_Comp_by_chr_start_end()!\n$@");
-    return([]);
-  }
+    };
+    if ($@){
+        warn("ERROR: SQL failed in GlovarAdaptor->fetch_Light_Base_Comp_by_chr_start_end()!\n$@");
+        return([]);
+    }
 
-  my @bases = ();
+    my @bases = ();
 
-  my $refs = $sth->fetchall_arrayref();
+    my $refs = $sth->fetchall_arrayref();
 
-  for my $ref(@$refs){
-    my($position,$genomic_base,$A_count,$G_count,$C_count,$T_count) = @$ref;
+    for my $ref(@$refs){
+        my($position,$genomic_base,$A_count,$G_count,$C_count,$T_count,$e0,$e1,$e2,$e3) = @$ref;
 
-    my $basecomp = Bio::EnsEMBL::GlovarBaseComp->new_fast({
-						 'position'     => $position - $slice_start + 1,
-						 'genomic_base' => $genomic_base,
-						 'alleles'      => {
-								    'T' => $T_count,
-								    'G' => $G_count,
-								    'A' => $A_count,
-								    'C' => $C_count,
-								   },
-						 '_gsf_strand'  => 1,
-							  });
-    push (@bases, $basecomp);
+        my $basecomp = Bio::EnsEMBL::ExternalData::Glovar::BaseComposition->new_fast({
+                'position'     => $position - $slice_start + 1,
+                'genomic_base' => $genomic_base,
+                'alleles'      => {
+                    'T' => $T_count,
+                    'G' => $G_count,
+                    'A' => $A_count,
+                    'C' => $C_count,
+                },
+                'ethnicity'    => {
+                    'unknown' => $e0,
+                    'Caucasian' => $e1,
+                    'Asian' => $e2,
+                    'African-American' => $e3,
+                },
+                '_gsf_strand'  => 1,
+                });
+        push (@bases, $basecomp);
 
-  }
-  return (\@bases);
+    }
+    &eprof_end('glovar_basecomp');
+    return (\@bases);
 }
 
 =head2 fetch_Base_Comp_by_chr_start_end

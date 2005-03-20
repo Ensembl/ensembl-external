5799d2346712e505f373aa3766c1796bcdfbfa4d
Stephen Searle <searle@sanger.ac.uk>
2005-03-20 17:55:23 +0000

    Work on fetching TranscriptVariations by consequence type

--- modules/Bio/EnsEMBL/ExternalData/SangerSNP/TranscriptVariationAdaptor.pm
+++ modules/Bio/EnsEMBL/ExternalData/SangerSNP/TranscriptVariationAdaptor.pm
@@ -53,17 +53,6 @@ sub coordinate_systems {
 }
 
   
-=head2 get_consequences
-
-  Arg[1]      : Bio::EnsEMBL::Variation::Variation object
-  Example     : $glovar_adaptor->get_consequences($var);
-  Description : Adds a TranscriptVariation object to the variation
-  Return type : none
-  Exceptions  : none
-  Caller      : $self
-
-=cut
-
 sub fetch_all_by_VariationFeatures {
   my ($self, $vf_ref) = @_;
 
@@ -107,6 +96,11 @@ sub fetch_all_by_VariationFeatures {
   #print "Finished query - now making features\n";
   my %trans_hash;
 
+  # Mark all variation features as having had tvs added
+  #foreach my $vf (@$vf_ref) {
+  #  $vf->{transcriptVariations} = [];
+  #}
+
   my @tvs;
   while (my $row = $sth->fetchrow_hashref) {
     #add to the variation feature object all the transcript variations
@@ -136,52 +130,86 @@ sub fetch_all_by_VariationFeatures {
   #print "Finished making features\n";
   return \@tvs;
 }
-#    my $q = qq(
-#        SELECT
-#                ptd.description         as pos_type,
-#                sgc_dict.description    as consequence,
-#                cs.name                 as transcript_stable_id,
-#                sgc.transcript_position as cdna_start
-#        FROM    
-#                coding_sequence cs,
-#                snp_gene_consequence sgc
-#        LEFT JOIN
-#                position_type_dict ptd on sgc.position_description = ptd.id_dict
-#        LEFT JOIN
-#                sgc_dict on sgc.consequence = sgc_dict.id_dict
-#        WHERE   sgc.id_snp = ?
-#        AND     sgc.id_codingseq = cs.id_codingseq
-#        AND     cs.design_entry = ?
-#    );
-#    my $sth;
-#    eval {
-#        $sth = $self->prepare($q);
-#        $sth->execute($varfeat->dbID, $self->consequence_exp);
-#    }; 
-#    if ($@){
-#        warn("ERROR: SQL failed in " . (caller(0))[3] . "\n$@");
-#        return;
-#    }
-#
-#    while (my $row = $sth->fetchrow_hashref) {
-#        print "Consequence for " . $varfeat->variation_name . " " . $row->{'POS_TYPE'} . " " . $row->{'CONSEQUENCE'} . "\n";
-#        # add consequence
-##        my $consequence_type = $CONSEQUENCE_TYPE_MAP{$row->{'POS_TYPE'}." ".$row->{'CONSEQUENCE'}};
-##        $varfeat->add_consequence_type($consequence_type);
-##
-##        # add TranscriptVariation object
-##        my $trans = Bio::EnsEMBL::Transcript->new(
-##            -STABLE_ID => $row->{'TRANSCRIPT_STABLE_ID'},
-##        );
-##        my $tvar = Bio::EnsEMBL::Variation::TranscriptVariation->new_fast({
-##            transcript          => $trans,
-##            cdna_start          => $row->{'CDNA_START'},
-##            cdna_end            => $row->{'CDNA_START'},
-##            consequence_type    => $consequence_type,
-##        });
-##        $varfeat->add_TranscriptVariation($tvar);
-#    }
-#}
+
+sub fetch_all_by_postype_consequence {
+  my ($self, $postype, $consequence) = @_;
+
+  my $q = qq(
+      SELECT
+              sgc.id_snp              as idsnp,
+              ptd.description         as pos_type,
+              sgc_dict.description    as consequence,
+              cs.name                 as transcript_stable_id,
+              sgc.transcript_position as cdna_start
+      FROM    
+              coding_sequence cs,
+              snp_gene_consequence sgc,
+              position_type_dict ptd,
+              sgc_dict
+      WHERE   sgc_dict.description = ?
+      AND     ptd.description = ?
+      AND     sgc.consequence = sgc_dict.id_dict
+      AND     sgc.position_description = ptd.id_dict
+      AND     sgc.id_codingseq = cs.id_codingseq
+      AND     cs.design_entry = ?
+  );
+  my $sth;
+  eval {
+      # print $q . "\n";
+      $sth = $self->prepare($q);
+      $sth->execute($consequence,$postype,$self->consequence_exp);
+  }; 
+  if ($@){
+      warn("ERROR: SQL failed in " . (caller(0))[3] . "\n$@");
+      return;
+  }
+
+  #print "Finished query - now making features\n";
+  my %trans_hash;
+  my %vf_hash;
+
+  my @tvs;
+  while (my $row = $sth->fetchrow_hashref) {
+    #add to the variation feature object all the transcript variations
+    my $conskey = $row->{'POS_TYPE'}." ".$row->{'CONSEQUENCE'};
+    my $consequence_type = $CONSEQUENCE_TYPE_MAP{$conskey};
+
+    # add TranscriptVariation object
+    my $tsid = $row->{TRANSCRIPT_STABLE_ID};
+    if (!exists($trans_hash{$tsid})) {
+      #print "fetching transcript $tsid\n";
+      $trans_hash{$tsid} = $self->ensembl_db->get_TranscriptAdaptor->fetch_by_stable_id($tsid);
+    }
+
+    my $vfid = $row->{IDSNP};
+    if (!exists($vf_hash{$vfid})) {
+      #print "fetching variation $vfid\n";
+      $vf_hash{$vfid} = $self->db->get_VariationAdaptor->fetch_by_dbID_position_range($vfid,$trans_hash{$tsid}->seq_region_name, $trans_hash{$tsid}->start, $trans_hash{$tsid}->end);
+    }
+
+    if (defined($vf_hash{$vfid})) {
+      my $tvar = Bio::EnsEMBL::Variation::TranscriptVariation->new_fast({
+             transcript          => $trans_hash{$tsid},
+             cdna_start          => $row->{CDNA_START},
+             cdna_end            => $row->{CDNA_START},
+             consequence_type    => $consequence_type,
+             variation_feature   => $vf_hash{$vfid},
+         });
+  
+  
+      $vf_hash{$vfid}->add_TranscriptVariation( $tvar );
+      $vf_hash{$vfid}->add_consequence_type($consequence_type);
+  
+      push @tvs,$tvar;
+    } else {
+      print "Missing variation feature " . $row->{IDSNP} . " for transcript " . $tsid . 
+            " " . $trans_hash{$tsid}->seq_region_name . " " . $trans_hash{$tsid}->start . " " . 
+            $trans_hash{$tsid}->end . "\n";
+    }
+  }
+  #print "Finished making features\n";
+  return \@tvs;
+}
 
 =head2 consequence_exp
 
--- modules/Bio/EnsEMBL/ExternalData/SangerSNP/VariationAdaptor.pm
+++ modules/Bio/EnsEMBL/ExternalData/SangerSNP/VariationAdaptor.pm
@@ -156,86 +156,97 @@ sub coordinate_systems {
   return ("ASSEMBLY");
 }
 
+sub fetch_by_dbID_position_range {
+  my ($self,$dbID,$range_chr,$range_start,$range_end) = @_;
+
+  my $assembly = $self->ensembl_db->assembly_type;
   
-=head2 get_consequences
+  (my $assembly_name = $assembly) =~ s/[0-9]*$//;
+  (my $assembly_version = $assembly) =~ s/[A-Z,a-z]*([0-9]*)$/$1/;
 
-  Arg[1]      : Bio::EnsEMBL::Variation::Variation object
-  Example     : $glovar_adaptor->get_consequences($var);
-  Description : Adds a TranscriptVariation object to the variation
-  Return type : none
-  Exceptions  : none
-  Caller      : $self
+  my $query = qq {
+SELECT DISTINCT MAPPED_SNP.ID_SNP,  
+          (MAPPED_SNP.POSITION + SEQ_SEQ_MAP.START_COORDINATE -1) AS snppos,
+          (MAPPED_SNP.END_POSITION + SEQ_SEQ_MAP.START_COORDINATE -1) AS snpendpos,
+          (MAPPED_SNP.IS_REVCOMP * SEQ_SEQ_MAP.CONTIG_ORIENTATION) AS snpstrand,
+           CHROM_SEQ.DATABASE_SEQNAME as chrname,
+           SNP_SUMMARY.ALLELES,
+           SNP_SUMMARY.DEFAULT_NAME
+FROM     DATABASE_DICT,
+         CHROM_SEQ,
+         SEQ_SEQ_MAP,
+         MAPPED_SNP,
+         SNP_SUMMARY
+WHERE     DATABASE_DICT.DATABASE_NAME = '$assembly_name'
+    AND   DATABASE_DICT.DATABASE_VERSION = '$assembly_version'
+    AND   CHROM_SEQ.DATABASE_SOURCE = DATABASE_DICT.ID_DICT
+    AND   CHROM_SEQ.IS_CURRENT = 1
+    AND   CHROM_SEQ.ID_CHROMSEQ = SEQ_SEQ_MAP.ID_CHROMSEQ
+    AND   MAPPED_SNP.ID_SEQUENCE =SEQ_SEQ_MAP.SUB_SEQUENCE
+    AND   SNP_SUMMARY.ID_SNP = MAPPED_SNP.ID_SNP
+    AND   MAPPED_SNP.IS_REVCOMP IS NOT NULL
+    AND   SNP_SUMMARY.ID_SNP = $dbID
+ORDER BY MAPPED_SNP.ID_SNP, SNPPOS
+  };
 
-=cut
+  my $sth = $self->prepare($query);
 
-sub get_consequences {
-    my ($self, $varfeat) = @_;
-    my $q = qq(
-        SELECT
-                ptd.description         as pos_type,
-                sgc_dict.description    as consequence,
-                cs.name                 as transcript_stable_id,
-                sgc.transcript_position as cdna_start
-        FROM    
-                coding_sequence cs,
-                snp_gene_consequence sgc
-        LEFT JOIN
-                position_type_dict ptd on sgc.position_description = ptd.id_dict
-        LEFT JOIN
-                sgc_dict on sgc.consequence = sgc_dict.id_dict
-        WHERE   sgc.id_snp = ?
-        AND     sgc.id_codingseq = cs.id_codingseq
-        AND     cs.design_entry = ?
-    );
-    my $sth;
-    eval {
-        $sth = $self->prepare($q);
-        $sth->execute($varfeat->dbID, $self->consequence_exp);
-    }; 
-    if ($@){
-        warn("ERROR: SQL failed in " . (caller(0))[3] . "\n$@");
-        return;
+  #print $sth->{Statement} . "\n";
+
+  $sth->execute;
+  # print "Query finished\n";
+
+  my @snps;
+
+# Naughty but should speed things up a bit
+  my $cur_snp_id = -1;
+  my $snp;
+  my %ids;
+  my $hashref;
+  while ($hashref = $sth->fetchrow_hashref) {
+    
+    my $start;
+    my $end;
+    if ($hashref->{SNPPOS} >= $hashref->{SNPENDPOS} ||
+       ($hashref->{ALLELES} =~ /-/ && abs($hashref->{SNPPOS}-$hashref->{SNPENDPOS})==1)) {
+      $start = $hashref->{SNPENDPOS};
+      $end = $hashref->{SNPPOS};
+    } else {
+      $start = $hashref->{SNPPOS};
+      $end = $hashref->{SNPENDPOS};
     }
 
-    while (my $row = $sth->fetchrow_hashref) {
-        print "Consequence for " . $varfeat->variation_name . " " . $row->{'POS_TYPE'} . " " . $row->{'CONSEQUENCE'} . "\n";
-        # add consequence
-#        my $consequence_type = $CONSEQUENCE_TYPE_MAP{$row->{'POS_TYPE'}." ".$row->{'CONSEQUENCE'}};
-#        $varfeat->add_consequence_type($consequence_type);
-#
-#        # add TranscriptVariation object
-#        my $trans = Bio::EnsEMBL::Transcript->new(
-#            -STABLE_ID => $row->{'TRANSCRIPT_STABLE_ID'},
-#        );
-#        my $tvar = Bio::EnsEMBL::Variation::TranscriptVariation->new_fast({
-#            transcript          => $trans,
-#            cdna_start          => $row->{'CDNA_START'},
-#            cdna_end            => $row->{'CDNA_START'},
-#            consequence_type    => $consequence_type,
-#        });
-#        $varfeat->add_TranscriptVariation($tvar);
+    if ($hashref->{CHRNAME} ne $range_chr || $start < $range_start || $start > $range_end) {
+      print "Outside range ($range_chr,$range_start,$range_end) for id $dbID " . $hashref->{CHRNAME} . " $start $end\n"; 
+      next;
     }
+    print "In range ($range_chr,$range_start,$range_end) for id $dbID " . $hashref->{CHRNAME} . " $start $end\n"; 
+    my $varfeat = Bio::EnsEMBL::Variation::VariationFeature->new_fast(
+      {
+        'dbID'              => $hashref->{ID_SNP},
+        'adaptor'           => $self,
+        'variation_name'    => $hashref->{DEFAULT_NAME},
+        'start'             => $start,
+        'end'               => $end,
+        'strand'            => $hashref->{SNPSTRAND},
+        'allele_string'     => $hashref->{ALLELES},
+        'source'            => 'SangerSNP',
+      });
+
+    # add minimal Variation object
+    my $var = Bio::EnsEMBL::Variation::Variation->new(
+        -dbID               => $hashref->{'ID_SNP'},
+        -ADAPTOR            => $self,
+        -NAME               => $hashref->{'DEFAULT_NAME'},
+        -SOURCE             => 'Glovar',
+      );
+
+    push @snps,$varfeat;
+  }
+
+  if (scalar(@snps) > 1) {
+    print STDERR "Got multiple vars for $dbID - only returning 1\n";
+  }
+  return $snps[0];
 }
-
-=head2 consequence_exp
-
-  Arg[1]      : (optional) consequence experiment id
-  Example     : $glovar_adaptor->consequence_ext(2046);
-  Description : getter/setter for the consequence experiment
-                (coding_sequence.design_entry in the glovar db)
-  Return type : String - consequence experiment id
-  Exceptions  : none
-  Caller      : general
-
-=cut
-
-sub consequence_exp {
-    my ($self, $exp) = @_;
-    if ($exp) {
-        $self->{'consequence_exp'} = $exp;
-    }
-    return $self->{'consequence_exp'};
-}
-
-
 1;

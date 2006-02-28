43f1d7d30587ce7e6eedb7a04b30892bae92b73a
Web Admin <w3adm@sanger.ac.uk>
2006-02-28 10:00:53 +0000

    added check for invalid URL

--- modules/Bio/EnsEMBL/ExternalData/DAS/DASAdaptor.pm
+++ modules/Bio/EnsEMBL/ExternalData/DAS/DASAdaptor.pm
@@ -138,6 +138,13 @@ sub new {
 #    $url      && $self->url( $url );
 #    $protocol && $self->protocol( $protocol );
 #    $domain   && $self->domain( $domain );
+   if ($dsn) {
+	if ($url =~ m/$dsn\/$dsn/) {
+	    $url =~ s/$dsn\/$dsn/$dsn/;
+	} elsif ($url =~ m/\/das$/) {
+	    $url .= "/$dsn";
+	}
+    }
 
     my $source_url = $self->url($url);
 

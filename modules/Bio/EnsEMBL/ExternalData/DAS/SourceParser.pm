01e2976d8a9a1bc9abe4b9d76e14797696fa4321
Andrew Jenkinson <aj5@sanger.ac.uk>
2008-07-29 09:12:37 +0000

    Reworked to use Bio::Das::Lite

--- modules/Bio/EnsEMBL/ExternalData/DAS/SourceParser.pm
+++ modules/Bio/EnsEMBL/ExternalData/DAS/SourceParser.pm
@@ -4,21 +4,17 @@ Bio::EnsEMBL::ExternalData::DAS::SourceParser
 
 =head1 SYNOPSIS
 
-  my $parser = Bio::EnsEMBL::ExternalData::DAS::SourceParser->new(-xml => $xml);
-  
-  OR:
-  
   my $parser = Bio::EnsEMBL::ExternalData::DAS::SourceParser->new(
-    -server  => $filename || 'http://www.dasregistry/das1',
-    -timeout => 5, # default
-    -proxy   => 'http://proxy.company.com',
+    -location => 'http://www.dasregistry/das1',
+    -timeout  => 5,
+    -proxy    => 'http://proxy.company.com',
   );
   
   my $sources = $parser->fetch_Sources( -taxid => 9606 );
   for my $source (@{ $sources }) {
     printf "URL: %s, Description: %s, Coords: %s\n",
             $source->full_url,
-            $source->description;,
+            $source->description,
             join '; ', @{ $source->coord_systems };
   }
 
@@ -43,6 +39,7 @@ use Bio::EnsEMBL::Utils::Argument  qw(rearrange);
 use Bio::EnsEMBL::Utils::Exception qw(throw warning info);
 use Bio::EnsEMBL::CoordSystem;
 use Bio::EnsEMBL::ExternalData::DAS::Source;
+use Bio::Das::Lite;
 
 # Intended for occasions when assembly names don't match between DAS and Ensembl
 # e.g. NCBIm37 -> NCBIM37
@@ -63,18 +60,21 @@ our $TYPE_MAPPINGS = {
 =head2 new
 
   Arg [..]   : List of named arguments:
-               -XML       - XML string to parse for sources
-               OR
-               -LOCATION  - URL/file from which to obtain XML
+               -LOCATION  - URL from which to obtain sources XML. This is
+                            usually a DAS registry/server URL, but could be a
+                            local path to a directory containing an XML file
+                            named "sources?".
                -PROXY     - Web proxy
-               -TIMEOUT   - Timeout in seconds (default is 5)
-  Example 1  : my $parser = Bio::EnsEMBL::ExternalData::DAS::SourceParser->new(
-                 -xml => '<SOURCES ....',
+               -TIMEOUT   - Timeout in seconds (default is 10)
+  Example    : my $parser = Bio::EnsEMBL::ExternalData::DAS::SourceParser->new(
+                 -location => 'http://www.dasregistry.org/das',
+                 -proxy    => 'http://proxy.company.com',
+                 -timeout  => 10,
                );
-  Example 2  : my $parser = Bio::EnsEMBL::ExternalData::DAS::SourceParser->new(
-                 -server  => 'http://www.dasregistry.org/das',
-                 -proxy   => 'http://proxy.company.com',
-                 -timeout => 10,
+  Example    : my $parser = Bio::EnsEMBL::ExternalData::DAS::SourceParser->new(
+                 -location => 'file:///registry', # parses "/registry/sources?"
+                 -proxy    => 'http://proxy.company.com',
+                 -timeout  => 10,
                );
   Description: Constructor
   Returntype : Bio::EnsEMBL::ExternalData::DAS::SourceParser
@@ -85,21 +85,19 @@ our $TYPE_MAPPINGS = {
 =cut
 sub new {
   my $class = shift;
-  my ($xml, $server, $proxy, $timeout) = rearrange([ 'XML', 'LOCATION', 'PROXY', 'TIMEOUT' ], @_);
+  my ($registry, $proxy, $timeout) = rearrange(['LOCATION','PROXY','TIMEOUT'], @_);
   
-  $timeout ||= 5;
+  $timeout ||= 10;
+  my $das = Bio::Das::Lite->new();
+  $das->user_agent('Ensembl');
+  $das->http_proxy($proxy);
+  $das->timeout($timeout);
+  $das->dsn($registry);
+  
   my $self = {
-    '_xml'    => $xml,
+    'daslite'    => $das,
   };
   bless $self, $class;
-  
-  if (!$xml) {
-    if ($server) {
-      $self->{'_xml'} = &_fetch_XML($server, $proxy, $timeout);
-    } else {
-      throw('No server or XML specified');
-    }
-  }
   
   return $self;
 }
@@ -117,7 +115,8 @@ sub new {
                to this method initiates lazy parsing of the XML, and the results
                are stored.
   Returntype : Arrayref of Bio::EnsEMBL::ExternalData::DAS::Source objects
-  Exceptions : If no taxonomy ID is specified
+  Exceptions : If no taxonomy ID is specified, or if there is an error
+               contacting the DAS registry/server.
   Caller     : general
   Status     : Stable
   
@@ -127,13 +126,13 @@ sub fetch_Sources {
   my ($f_taxid, $f_name) = rearrange([ 'TAXID', 'NAME' ], @_);
   $f_taxid || throw('No taxonomy ID specified');
   
-  # XML parsing is lazy
+  # Actual parsing is lazy
   if (!defined $self->{'_sources'}) {
-    $self->{'_sources'} = &_parse_XML($self->{'_xml'});
+    $self->{'_sources'} = $self->_parse_registry();
   }
   
   my $sources = $self->{'_sources'}->{$f_taxid}
-             || $self->{'_sources'}->{'__ALL__'};
+             || $self->{'_sources'}->{'__NONE__'};
   
   # optional name filter
   if ($f_name) {
@@ -146,166 +145,118 @@ sub fetch_Sources {
 
 =head1 SUBROUTINES
 
-=head2 _parse_XML
+=head2 _parse_registry
 
-  Args [1]   : XML string to parse
-  Example    : $hash = $parser->_parse_XML($xml);
-  Description: Creates Source objects by parsing XML.
-  Returntype : hashref of taxid => Bio::EnsEMBL::ExternalData::DAS::Source arrayref
-  Exceptions : If no XML is specified
+  Arg [..]   : none
+  Example:     $hash = $parser->_parse_registry;
+  Description: Contacts the configured DAS registry via the sources command and
+               parses the results.
+  Returntype : Hashref of DAS sources, organised by taxonomy ID:
+               {
+                9606     => [ Bio::EnsEMBL::ExternalData::DAS::Source, ... ],
+                __NONE__ => [ Bio::EnsEMBL::ExternalData::DAS::Source, ... ],
+               }
+  Exceptions : If there is an error contacting the DAS registry/server.
   Caller     : general
   Status     : Stable
 
 =cut
-sub _parse_XML {
-  my $xml  = shift;
-  $xml    || throw('No XML to parse');
+sub _parse_registry {
+  my $self = shift;
+  my %parsed = ( );
   
-  my %sources = ();
+  # Use Bio::Das::Lite to perform the request
+  my $struct = $self->{'daslite'}->sources();
   
-  # Split by source
-  for my $s_xml (split /<SOURCE\s/, $xml) {
-    my ($av_xml) = $s_xml =~ m|(<VERSION.+</VERSION>)|s; # all the versions
-    $av_xml || next;
-    my ($title) = $s_xml =~ m|title="(.*?)"|;
-    my ($homepage) = $s_xml =~ m|doc_href="(.*?)"|;
-    my ($description) = $s_xml =~ m|description="(.*?)"|;
-    my ($email) = $s_xml =~ m|<MAINTAINER\s+email="(.*?)"|;
+  # Iterate over each registry (really we expect only one)
+  while (my ($url, $set) = each %{ $struct }) {
+    my $status = $self->{'daslite'}->statuscodes($url);
+    if ($status !~ /^200/) {
+      throw("Error contacting server: $status");
+    }
+    $set = $set->[0]->{'source'};
     
-    # Split by version
-    for my $v_xml (split m|</VERSION>|, $av_xml) {
+    # Iterate over the <SOURCE> elements
+    for my $source (@{ $set || [] }) {
       
-      # Skip sources without support for features command
-      if ($v_xml !~ m|type="das1:features"|) {
-        next;
-      }
+      my $title       = $source->{'source_title'};
+      my $homepage    = $source->{'source_doc_href'};
+      my $description = $source->{'source_description'};
+      my $email       = $source->{'maintainer'}[0]{'maintainer_email'};
       
-      # Extract URL and DSN
-      my ($url, $dsn) = $v_xml =~ m|query_uri="([^>]+/das1?)/([^>]+)/features"|;
-      if (!$dsn || !$url) {
-        warning("Unable to parse dsn and URL"); # Something went wrong!
-        next;
-      }
-      
-      info("Parsing $dsn");
-      
-      # Now parse the coordinate systems and map to Ensembl's
-      my %coords = ( '__ALL__' => [] );
-      ($v_xml) = $v_xml =~ m|(<COORDINATES.+</COORDINATES>)|s; # all the coordinates
-      for my $c_xml (split m|</COORDINATES>|, $v_xml||q()) {
+      # Iterate over the <VERSION> elements
+      for my $version (@{ $source->{'version'} || [] }) {
         
-        # Extract coordinate details
-        my ($auth)    = $c_xml =~ m|authority="(.*?)"|;
-        my ($type)    = $c_xml =~ m|source="(.*?)"|;
-        my ($taxid)   = $c_xml =~ m|taxid="(.*?)"|;   # optional
-        my ($version) = $c_xml =~ m|version="(.*?)"|; # optional
-        
-        if (!$type || !$auth) {
-          warning("Unable to parse authority and sequence type for $dsn");warn $c_xml ;# Something went wrong!
-          next;
-        }
-        $type = $TYPE_MAPPINGS->{$type} || lc $type; # handle fringe cases
-        
-        # Wizardry to convert to Ensembl coord_system
-        my $coord;
-        if ($type =~ m/^chromosome|clone|contig|scaffold|supercontig$/) {
-          # seq_region coordinate systems have ensembl equivalents
-          $version ||= q();
-          $version = $auth.$version;
-          $version = $ASSEMBLY_MAPPINGS->{$version} || $version; # handle fringe cases
-          $coord = "$type:$version";
-        } else {
-          # otherwise use a 'fake' coordinate system like 'ensembl_gene'
-          $coord = lc $auth.q(_).$type;
-        }
-        
-        if ($taxid) {
-          $coords{$taxid} ||= [@{ $coords{'__ALL__'} }];
-          push(@{ $coords{$taxid} }, $coord);
-        } else {
-          for (values %coords) {
-            push(@{ $_ }, $coord);
+        my ($url, $dsn);
+        for my $cap (@{ $version->{'capability'} || [] }) {
+          if ($cap->{'capability_type'} eq 'das1:features') {
+            ($url, $dsn) = $cap->{capability_query_uri} =~ m|(.+/das1?)/(.+)/features|;
+            last;
           }
         }
-      }
+        $dsn || next; # this source doesn't support features command
+        
+        # Now parse the coordinate systems and map to Ensembl's
+        my %coords = ( '__NONE__' => [] );
+        for my $coord (@{ $version->{'coordinates'} || [] }) {
+          
+          # Extract coordinate details
+          my $auth    = $coord->{'coordinates_authority'};
+          my $type    = $coord->{'coordinates_source'};
+          my $taxid   = $coord->{'coordinates_taxid'};   # optional
+          my $version = $coord->{'coordinates_version'}; # optional
+          
+          if (!$type || !$auth) {
+            warning("Unable to parse authority and sequence type for $dsn"); ;# Something went wrong!
+            next;
+          }
+          
+          $type = $TYPE_MAPPINGS->{$type} || lc $type; # handle fringe cases
+           
+           # Wizardry to convert to Ensembl coord_system
+           my $coord;
+           if ($type =~ m/^chromosome|clone|contig|scaffold|supercontig$/) {
+             # seq_region coordinate systems have ensembl equivalents
+             $version ||= q();
+             $version = $auth.$version;
+             $version = $ASSEMBLY_MAPPINGS->{$version} || $version; # handle fringe cases
+             $coord = "$type:$version";
+           } else {
+             # otherwise use a 'fake' coordinate system like 'ensembl_gene'
+             $coord = lc $auth.q(_).$type;
+           }
+           
+          $taxid ||= '__NONE__';
+          $coords{$taxid} ||= [];
+          push(@{ $coords{$taxid} }, $coord);
+        }
+        
+        if (!scalar keys %coords) {
+          warning("$dsn has no coordinate systems");
+        }
+        
+        # Create the actual sources, one per species plus one for all species
+        while (my ($taxid, $coords) = each %coords) {
+          my $source = Bio::EnsEMBL::ExternalData::DAS::Source->new(
+            -display_label => $title || $dsn,
+            -url           => $url,
+            -dsn           => $dsn,
+            -maintainer    => $email,
+            -homepage      => $homepage || $url,
+            -description   => $description,
+            -coords        => $taxid eq '__NONE__' ? $coords : [ @{ $coords{'__NONE__'} }, @{ $coords } ] ,
+          );
+          $parsed{$taxid} ||= [];
+          push(@{ $parsed{$taxid} }, $source);
+        } # end coord loop
+        
+      } # end version loop
       
-      if (!scalar keys %coords) {
-        warning("$dsn has no coordinate systems");
-      }
-      
-      # Create the actual sources, one per species plus one for all species
-      while (my ($taxid, $coords) = each %coords) {
-        @{ $coords }     ||  next;
-        $sources{$taxid} ||= [];
-        my $source = Bio::EnsEMBL::ExternalData::DAS::Source->new(
-          -display_label => $title || $dsn,
-          -url           => $url,
-          -dsn           => $dsn,
-          -maintainer    => $email,
-          -homepage      => $homepage || $url,
-          -description   => $description,
-          -coords        => $coords,
-        );
-        push(@{ $sources{$taxid} }, $source);
-      }
-    }
-  }
+    } # end source loop
+    
+  } # end set loop
   
-  return \%sources;
-}
-
-# Server can be any server supporting the sources command (e.g. registry)
-# Or can be a file
-=head2 _fetch_XML
-
-  Args [1]   : Location, either a filename or URL
-  Args [2]   : (optional) proxy URL
-  Args [3]   : (optional) timeout
-  Example    : _fetch_XML($location, $proxy, $timeout);
-  Description: Retrieves XML from a local (file) or remote (URL) source.
-  Returntype : None
-  Exceptions : If no server/file is specified
-  Caller     : general
-  Status     : Stable
-
-=cut
-sub _fetch_XML {
-  my $server  = shift;
-  my $proxy   = shift;
-  my $timeout = shift;
-  
-  $server || throw('No server specified');
-  
-  # Can be a file, in which case we expect it to exist...
-  if (-e $server) {
-    info("Using local DAS registry XML file: $server");
-    my $fh;
-    open $fh, '<', $server;
-    local $/ = undef;
-    my $xml = <$fh>;
-    close $fh;
-    return $xml;
-  }
-  
-  info("Using remote DAS registry URL: $server");
-  require LWP::UserAgent;
-  if ($server !~ m|/sources/?$|) {
-    $server = $server =~ m|/$| ? $server.'sources' : $server.'/sources';
-  }
-  
-  my $ua = LWP::UserAgent->new(
-    -agent   => 'Ensembl',
-    -timeout => $timeout,
-  );
-  $ua->proxy(['http','https'], $proxy) if $proxy;
-  
-  my $response = $ua->get($server);
-  if ($response->is_success) {
-    return $response->content;
-  }
-  
-  warning("Unable to contact $server: ".$response->status_line);
-  return undef;
+  return \%parsed;
 }
 
 1;

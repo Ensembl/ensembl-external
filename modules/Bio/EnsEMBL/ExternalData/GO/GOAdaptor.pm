4dd80aa6165b30da21f099d9fdfe4f4dc4d8eddc
Glenn Proctor <gp1@sanger.ac.uk>
2008-08-15 14:39:58 +0000

    Remove debug.

--- modules/Bio/EnsEMBL/ExternalData/GO/GOAdaptor.pm
+++ modules/Bio/EnsEMBL/ExternalData/GO/GOAdaptor.pm
@@ -118,7 +118,6 @@ sub new {
     }
     if(! $pass ) { $pass =''; } 
     my $dbh ; eval { $dbh= GO::AppHandle->connect(-dbname=>$db,-dbhost=>"$host:$port",-dbuser=>$user,($pass?(-dbauth=>$pass):())); };
-print "$host $port $user $pass\n";
     throw("Could not connect to database $db user $user as a locator ($@)") unless $dbh;
     $self->_db_handle($dbh);
     

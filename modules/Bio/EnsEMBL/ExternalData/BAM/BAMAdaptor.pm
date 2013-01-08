e42c06bcf787014a80d314706a19e6ea2d3e9b4c
Stephen Searle <searle@sanger.ac.uk>
2013-01-08 20:23:51 +0000

    Added extra checks for '-'s in fetch_consensus to fix bug in consensus (didn't display '-' when '-' was the consensus)

--- modules/Bio/EnsEMBL/ExternalData/BAM/BAMAdaptor.pm
+++ modules/Bio/EnsEMBL/ExternalData/BAM/BAMAdaptor.pm
@@ -264,7 +264,7 @@ sub fetch_consensus {
     my ($total, $different);
     my $qhash;
     for my $pileup (@$p) {
-      if ($pileup->indel) {
+      if ($pileup->indel || $pileup->is_refskip || $pileup->is_del) {
         $qhash->{'-'}++;
         next;
       }
